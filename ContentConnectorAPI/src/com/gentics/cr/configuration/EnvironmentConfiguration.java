805c5e155b94b871e6dca4e02e469629f2ee157c
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-06-22 15:46:34 +0000

    Added Generic Config

---
+++ ContentConnectorAPI/src/com/gentics/cr/configuration/EnvironmentConfiguration.java
@@ -0,0 +1,85 @@
+package com.gentics.cr.configuration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.jcs.engine.control.CompositeCacheManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.gentics.cr.util.CRUtil;
+
+
+
+/**
+ * 
+ * Last changed: $Date: 2009-06-09 14:33:13 +0200 (Di, 09 Jun 2009) $
+ * @version $Revision: 79 $
+ * @author $Author: supnig@constantinopel.at $
+ *
+ */
+public class EnvironmentConfiguration {
+	
+	private static final String LOGGER_FILE_PATH = "${com.gentics.portalnode.confpath}/nodelog.properties";
+	private static final String CACHE_FILE_PATH = "${com.gentics.portalnode.confpath}/cache.ccf";
+	
+	private static Logger log = Logger.getLogger(EnvironmentConfiguration.class);
+	
+	/**
+	 * Load Environment Properties
+	 * 		- load logger properties for log4j
+	 * 		- load chache properties for JCS
+	 */
+	public static void loadEnvironmentProperties()
+	{
+		loadLoggerPropperties();
+		loadCacheProperties();
+	}
+	
+	/**
+	 * Load Property file for Log4J
+	 */
+	public static void loadLoggerPropperties()
+	{
+		Properties logprops = new Properties();
+		try {
+			if(CRUtil.resolveSystemProperties("${com.gentics.portalnode.confpath}").equals("")){
+				System.setProperty("com.gentics.portalnode.confpath", System.getProperty("catalina.base")+File.separator+"conf"+File.separator+"gentics"+File.separator);
+			}
+			String confpath = CRUtil.resolveSystemProperties(LOGGER_FILE_PATH);
+			//System.out.println("TRYING TO LOAD NODELOGPROPS FROM: "+confpath);
+			logprops.load(new FileInputStream(confpath));
+			PropertyConfigurator.configure(logprops);
+		} catch (IOException e) {
+			log.error("Could not find nodelog.properties.");
+			//e.printStackTrace();
+		}catch (NullPointerException e) {
+			log.error("Could not find nodelog.properties.");
+			//e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Load Property file for JCS chache
+	 */
+	public static void loadCacheProperties()
+	{
+		try {
+			//LOAD CACHE CONFIGURATION
+			String confpath = CRUtil.resolveSystemProperties(CACHE_FILE_PATH);
+			Properties cache_props = new Properties();
+			cache_props.load(new FileInputStream(confpath));
+			CompositeCacheManager cManager = CompositeCacheManager.getUnconfiguredInstance();
+			cManager.configure(cache_props);
+		} catch(NullPointerException e){
+			log.error("Could not load cache configuration. Perhaps you are missing the file cache.ccf in "+CRUtil.resolveSystemProperties("${com.gentics.portalnode.confpath}/")+"!");
+		} catch (FileNotFoundException e) {
+			log.error("Could not load cache configuration. Perhaps you are missing the file cache.ccf in "+CRUtil.resolveSystemProperties("${com.gentics.portalnode.confpath}/")+"!");
+		} catch (IOException e) {
+			log.error("Could not load cache configuration. Perhaps you are missing the file cache.ccf in "+CRUtil.resolveSystemProperties("${com.gentics.portalnode.confpath}/")+"!");
+		}
+	}
+}
--- ContentConnectorAPI/src/com/gentics/cr/configuration/GenericConfiguration.java
+++ ContentConnectorAPI/src/com/gentics/cr/configuration/GenericConfiguration.java
@@ -1,8 +1,15 @@
 package com.gentics.cr.configuration;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.Vector;
+import java.util.Map.Entry;
 
 /**
+ * Stores a Configuration tree
+ * 		- Keys are stored in Upper Case
  * 
  * Last changed: $Date: 2009-06-09 14:33:13 +0200 (Di, 09 Jun 2009) $
  * @version $Revision: 79 $
@@ -11,12 +18,147 @@ import java.util.HashMap;
  */
 public class GenericConfiguration {
 	
-	private HashMap<String,String> properties;
-	private HashMap<String,GenericConfiguration> subconfigs;
+	protected Properties properties;
+	protected Hashtable<String,GenericConfiguration> subconfigs;
 	
+	/**
+	 * Create new instance of Generic Configuration
+	 */
 	public GenericConfiguration()
 	{
 		
+	}
+	
+	/**
+	 * Returns the containing properties as flat Properties class
+	 * 			- will not resolve to sub configurations
+	 * @return properties or null if there are no containing properties
+	 */
+	public Properties getProperties()
+	{
+		return(this.properties);
+	}
+	
+	/**
+	 * Returns rebuilt property tree as flat property file
+	 * @return flattened property file
+	 */
+	public Properties getRebuiltPropertyTree()
+	{
+		Properties ret = new Properties();
+		if(this.properties!=null)
+		{
+			for(Entry<Object,Object> e:this.properties.entrySet())
+			{
+				ret.put(e.getKey(), e.getValue());
+			}
+		}
+		if(this.subconfigs!=null)
+		{
+			for(Entry<Object,Object> e:this.properties.entrySet())
+			{
+				String subConfKey = (String)e.getKey();
+				Object subVal = e.getValue();
+				if(subVal!=null && subVal instanceof GenericConfiguration)
+				{
+					for(Entry<Object,Object> se:((GenericConfiguration)subVal).getRebuiltPropertyTree().entrySet())
+					{
+						String K = subConfKey+"."+se.getKey();
+						ret.put(K, se.getValue());
+					}
+				}
+				else
+				{
+					ret.put(subConfKey, subVal);
+				}
+			}
+		}
+		return(ret);
+	}
+	
+	/**
+	 * Returns the containing sub configurations
+	 * 			
+	 * @return hashtable of configurations with keys or null if there are no containing sub configs
+	 */
+	public Hashtable<String,GenericConfiguration> getSubConfigs()
+	{
+		return this.subconfigs;
+	}
+	
+	/**
+	 * Returns size of properties in this instance
+	 * 		- does not count sub configurations
+	 * @return count
+	 */
+	public int getPropertySize()
+	{
+		if(this.properties!=null)
+		{
+			return this.properties.size();
+		}
+		return(0);
+	}
+	
+	/**
+	 * Returns size of sub configs in this instance
+	 * 		- does not count properties
+	 * @return count
+	 */
+	public int getSubConfigSize()
+	{
+		if(this.subconfigs!=null)
+		{
+			return(this.subconfigs.size());
+		}
+		return(0);
+	}
+	
+	/**
+	 * Returns size of properties in this instance
+	 * 		- does count sub configurations
+	 * @return count
+	 */
+	public int getChildSize()
+	{
+		return(getSubConfigSize()+getPropertySize());
+	}
+	
+	/**
+	 * Gets flat properties in the resolved instance as sorted collection
+	 * @param key
+	 * @return collection of properties or null if no properties set or resolved object is not able to contain properties
+	 */
+	public ArrayList<String> getPropertiesAsSortedCollection(String key)
+	{
+		Object obj = get(key);
+		if(obj!=null && obj instanceof GenericConfiguration)
+		{
+			return ((GenericConfiguration)obj).getPropertiesAsSortedCollection();
+		}
+		return(null);
+	}
+	
+	/**
+	 * Gets flat properties in this instance as sorted collection
+	 * @return Collection of String or null if there are no properties
+	 */
+	@SuppressWarnings("unchecked")
+	public ArrayList<String> getPropertiesAsSortedCollection()
+	{
+		if(this.properties!=null)
+		{
+			ArrayList<String> ret = new ArrayList<String>(this.properties.size());
+			Vector v = new Vector(this.properties.keySet());
+			Collections.sort(v);
+			for(Object k:v)
+			{
+				ret.add(this.properties.getProperty((String)k));
+			}
+			return(ret);
+		}
+		return(null);
+
 	}
 	
 	/**
@@ -31,10 +173,11 @@ public class GenericConfiguration {
 	 * 			this config
 	 * 					-property config1
 	 * @param key
-	 * @return property value as string
+	 * @return property value as string or a GenericConfiguration object if key points to a config
 	 */
-	public String get(String key)
+	public Object get(String key)
 	{
+		key=convertKey(key);
 		if(isSubKey(key))
 		{
 			if(this.subconfigs!=null)
@@ -46,9 +189,12 @@ public class GenericConfiguration {
 				}
 			}
 		}
-		else if(this.properties!=null)
+		else if(this.properties!=null || this.subconfigs!=null)
 		{
-			return properties.get(key);
+			Object prop = null;
+			if(properties!=null)prop=properties.getProperty(key);
+			if(prop==null && subconfigs!=null)prop = subconfigs.get(key);
+			return(prop);
 		}
 		return null;
 	}
@@ -71,17 +217,18 @@ public class GenericConfiguration {
 	 */
 	public void set(String key, String value)
 	{
+		key=convertKey(key);
 		if(isSubKey(key))
 		{
-			if(this.subconfigs==null)this.subconfigs = new HashMap<String,GenericConfiguration>();
+			if(this.subconfigs==null)this.subconfigs = new Hashtable<String,GenericConfiguration>();
 			String confKey = getSubConfigKey(key);
 			if(this.subconfigs.get(confKey)==null)this.subconfigs.put(confKey, new GenericConfiguration());
 			this.subconfigs.get(confKey).set(getSubKey(key), value);
 		}
 		else
 		{
-			if(this.properties==null)this.properties = new HashMap<String,String>();
-			this.properties.put(key, value);
+			if(this.properties==null)this.properties = new Properties();
+			this.properties.setProperty(key, value);
 		}
 	}
 	
@@ -95,7 +242,7 @@ public class GenericConfiguration {
 		boolean ret = false;
 		if(key!=null)
 		{
-			String[] arr = key.split(".");
+			String[] arr = key.split("\\.");
 			if(arr!=null && arr.length>1)
 			{
 				ret=true;
@@ -115,11 +262,14 @@ public class GenericConfiguration {
 		
 		if(key!=null)
 		{
-			String[] arr = key.split(".");
+			String[] arr = key.split("\\.");
 			if(arr!=null && arr.length>1)
 			{
-				int firstpos = key.indexOf(".");
-				sub = key.substring(firstpos);
+				int firstpos = key.indexOf(".")+1;
+				if(key.length()>firstpos)
+				{
+					sub = key.substring(firstpos);
+				}
 			}
 		}
 		return(sub);
@@ -136,12 +286,23 @@ public class GenericConfiguration {
 		
 		if(key!=null)
 		{
-			String[] arr = key.split(".");
+			String[] arr = key.split("\\.");
 			if(arr!=null && arr.length>1)
 			{
 				sub=arr[0];
 			}
 		}
 		return(sub);
+	}
+	
+	/**
+	 * Prepares key for storage and fetching
+	 * 		- converty key to upper case
+	 * @param key
+	 * @return
+	 */
+	private String convertKey(String key)
+	{
+		return key.toUpperCase();
 	}
 }
