ed756b5fa7d0e4818a19da3105779cca6c3d44a2
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-06-29 15:40:05 +0000

    added plink stripper

---
+++ ContentConnectorAPI/src/com/gentics/cr/plink/PLinkStripper.java
@@ -0,0 +1,26 @@
+package com.gentics.cr.plink;
+
+import com.gentics.api.portalnode.connector.PLinkInformation;
+import com.gentics.api.portalnode.connector.PLinkReplacer;
+
+/**
+ *  Helper Class to be passed to PortalConnectorHelper.replacePLinks.
+ *  Replaces PLinks with "" => useful for indexing
+ *
+ * 
+ * Last changed: $Date: 2009-06-22 17:49:58 +0200 (Mo, 22 Jun 2009) $
+ * @version $Revision: 95 $
+ * @author $Author: supnig@constantinopel.at $
+ *
+ */
+public class PLinkStripper implements PLinkReplacer{
+
+	/**
+	 * Returns an empty String to remove PLinks in the Content
+	 */
+	@Override
+	public String replacePLink(PLinkInformation arg0) {
+		return "";
+	}
+
+}
