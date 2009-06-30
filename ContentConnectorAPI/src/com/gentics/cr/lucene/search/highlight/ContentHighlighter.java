a03e1e25c1bbb4cd8f5b9ea9885f08c5b402fb89
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-06-30 15:39:15 +0000

    Added highlighter and multivalue indexer for multi value strings

---
+++ ContentConnectorAPI/src/com/gentics/cr/lucene/search/highlight/ContentHighlighter.java
@@ -0,0 +1,127 @@
+package com.gentics.cr.lucene.search.highlight;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.apache.lucene.search.Query;
+
+import com.gentics.api.lib.exception.ParserException;
+import com.gentics.api.lib.expressionparser.Expression;
+import com.gentics.api.lib.expressionparser.ExpressionEvaluator;
+import com.gentics.api.lib.expressionparser.ExpressionParser;
+import com.gentics.api.lib.expressionparser.ExpressionParserException;
+import com.gentics.api.lib.resolving.Resolvable;
+import com.gentics.cr.configuration.GenericConfiguration;
+
+/**
+ * 
+ * Last changed: $Date: 2009-06-26 15:48:16 +0200 (Fr, 26 Jun 2009) $
+ * @version $Revision: 105 $
+ * @author $Author: supnig@constantinopel.at $
+ *
+ */
+public abstract class ContentHighlighter {
+
+	protected static Logger log = Logger.getLogger(ContentHighlighter.class);
+	private static ExpressionEvaluator evaluator = new ExpressionEvaluator();
+	private Expression expr;
+	
+	private String highlightAttribute = null;
+	
+	private static final String HIGHLIGHTER_ATTRIBUTE_KEY="attribute";
+	private static final String HIGHLIGHTER_RULE_KEY="rule";
+	
+	/**
+	 * Returns the hightlight attribute that is highlighted by this contentHighlighter
+	 * @return
+	 */
+	public String getHighlightAttribute()
+	{
+		return(highlightAttribute);
+	}
+	
+	protected ContentHighlighter(GenericConfiguration config)
+	{
+		String rule = (String)config.get(HIGHLIGHTER_RULE_KEY);
+		highlightAttribute = (String)config.get(HIGHLIGHTER_ATTRIBUTE_KEY);
+		try {
+			expr = ExpressionParser.getInstance().parse(rule);
+		} catch (ParserException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Highlights the configured attribute of a CRResolvableBean
+	 * @param attribute
+	 * @param parsedQuery
+	 * @return 
+	 */
+	public abstract String highlight(String attribute, Query parsedQuery);
+	
+	/**
+	 * Match an Resolvable to a Rule
+	 * @param object
+	 * @param rule
+	 * @return true if rule matches
+	 */
+	public boolean match(Resolvable object)
+	{
+		if(object!=null)
+		{
+			try {
+				return(evaluator.match(expr, object));
+			}catch (ExpressionParserException e) {
+				e.printStackTrace();
+			}
+		}
+		return(false);
+	}
+	
+	
+	private static final String HIGHLIGHTER_CLASS_KEY="class";
+	private static final String HIGHLIGHTER_KEY="highlighter";
+	
+	/**
+	 * Create table of ContentTransformers configured in config
+	 * @param config
+	 * @return
+	 */
+	public static Hashtable<String,ContentHighlighter> getTransformerTable(GenericConfiguration config)
+	{
+		GenericConfiguration tconf = (GenericConfiguration)config.get(HIGHLIGHTER_KEY);
+		if(tconf!=null)
+		{
+			Hashtable<String,GenericConfiguration> confs = tconf.getSubConfigs();
+			if(confs!=null && confs.size()>0)
+			{
+				Hashtable<String,ContentHighlighter> ret = new Hashtable<String,ContentHighlighter>(confs.size());
+				for(Map.Entry<String,GenericConfiguration> e:confs.entrySet())
+				{
+					try
+					{
+						GenericConfiguration c = e.getValue();
+						String attribute = (String)c.get(HIGHLIGHTER_ATTRIBUTE_KEY);
+						String highlighterClass = (String)c.get(HIGHLIGHTER_CLASS_KEY);
+						ContentHighlighter t = null;
+						t = (ContentHighlighter) Class.forName(highlighterClass).getConstructor(new Class[] {GenericConfiguration.class}).newInstance(c);
+						if(t!=null && attribute!=null)
+						{
+							ret.put(attribute, t);
+						}
+					}
+					catch(Exception ex)
+					{
+						log.error("Invalid configuration found. Could not construct ContentHighlighter Map");
+						ex.printStackTrace();
+					}
+					
+				}
+				return(ret);
+			}
+		}
+		
+		return null;
+	}
+}
---
+++ ContentConnectorAPI/src/com/gentics/cr/lucene/search/highlight/PhraseBolder.java
@@ -0,0 +1,133 @@
+package com.gentics.cr.lucene.search.highlight;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.highlight.Formatter;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.TextFragment;
+import org.apache.lucene.search.highlight.TokenGroup;
+
+import com.gentics.cr.configuration.GenericConfiguration;
+/**
+ * 
+ * Last changed: $Date: 2009-06-26 15:48:16 +0200 (Fr, 26 Jun 2009) $
+ * @version $Revision: 105 $
+ * @author $Author: supnig@constantinopel.at $
+ *
+ */
+public class PhraseBolder extends ContentHighlighter implements Formatter{
+
+	private Analyzer analyzer=null;
+	private int numMaxFragments=3;
+	private int fragmentSize=100;
+	
+	private String highlightPrefix="";
+	private String highlightPostfix="";
+	private String fragmentSeperator="";
+	
+	private static final String NUM_MAX_FRAGMENTS_KEY = "fragments";
+	private static final String NUM_FRAGMENT_SIZE_KEY = "fragmentsize";
+	private static final String PHRASE_PREFIX_KEY = "highlightprefix";
+	private static final String PHRASE_POSTFIX_KEY = "highlightpostfix";
+	private static final String FRAGMENT_SEPERATOR_KEY = "fragmentseperator";
+	/**
+	 * Create new Instance of PhraseBolder
+	 * @param config
+	 */
+	public PhraseBolder(GenericConfiguration config) {
+		super(config);
+		analyzer = new StandardAnalyzer();
+		
+		highlightPrefix = (String)config.get(PHRASE_PREFIX_KEY);
+		if(highlightPrefix==null)highlightPrefix="<b>";
+		highlightPostfix = (String)config.get(PHRASE_POSTFIX_KEY);
+		if(highlightPostfix==null)highlightPostfix="</b>";
+		fragmentSeperator = (String)config.get(FRAGMENT_SEPERATOR_KEY);
+		if(fragmentSeperator==null)fragmentSeperator="...";
+		
+		String nmF = (String)config.get(NUM_MAX_FRAGMENTS_KEY);
+		if(nmF!=null)
+		{
+			try
+			{
+				int i = Integer.parseInt(nmF);
+				numMaxFragments = i;
+			}catch(NumberFormatException e)
+			{
+				log.error("The configured count of fragments for this ContentHighlighter is not a number");
+			}
+		}
+		String nFS = (String)config.get(NUM_FRAGMENT_SIZE_KEY);
+		if(nFS!=null)
+		{
+			try
+			{
+				int i = Integer.parseInt(nFS);
+				fragmentSize = i;
+			}catch(NumberFormatException e)
+			{
+				log.error("The configured size for fragments for this ContentHighlighter is not a number");
+			}
+		}
+	}
+
+	/**
+	 * Highlights Terms by enclosing them with &lt;b&gt;term&lt;/b&gt;
+	 */
+	@Override
+	public String highlightTerm(String originalTermText, TokenGroup tokenGroup) {
+		
+	  
+	    if (tokenGroup.getTotalScore() <= 0) 
+	      return originalTermText;
+	  
+	    return highlightPrefix + originalTermText + highlightPostfix;
+
+	}
+
+	
+	/**
+	 * 
+	 */
+	@Override
+	public String highlight(String attribute, Query parsedQuery) {
+		String result="";
+		if(attribute!=null && parsedQuery!=null)
+		{
+			Highlighter highlighter = new Highlighter(this,new QueryScorer(parsedQuery));
+			highlighter.setTextFragmenter(new WordCountFragmenter(fragmentSize));
+			
+			TokenStream tokenStream = analyzer.tokenStream(this.getHighlightAttribute(), new StringReader(attribute));
+	
+			
+			try {
+				TextFragment[] frags = highlighter.getBestTextFragments(tokenStream, attribute, true, numMaxFragments);
+				boolean first=true;
+				for(TextFragment frag:frags)
+				{
+					if(!first)
+					{
+						result +=fragmentSeperator;
+					}
+					else
+					{
+						first=false;
+					}
+					result +=frag.toString();
+				}
+				//result = highlighter.getBestFragments(tokenStream,attribute,maxNumFragmentsRequired,fragmentSeparator);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return(result);
+	}
+
+}
