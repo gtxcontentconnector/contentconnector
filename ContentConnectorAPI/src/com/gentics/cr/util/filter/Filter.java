42683ceb00767bc83544ab67421ecf0406de2322
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-07-10 10:52:03 +0000

    added javadoc

--- ContentConnectorAPI/src/com/gentics/cr/util/filter/Filter.java
+++ ContentConnectorAPI/src/com/gentics/cr/util/filter/Filter.java
@@ -9,6 +9,7 @@ import com.gentics.api.lib.resolving.Resolvable;
  * Last changed: $Date$
  * @version $Revision$
  * @author $Author$
+ * @param <T> Any type of resolvable
  *
  */
 public interface Filter<T extends Resolvable> {
@@ -18,8 +19,8 @@ public interface Filter<T extends Resolvable> {
 	 * Implementations suggested:
 	 * - SortFilter
 	 * - PermissionFilter
-	 * @param objects: Collection of Resolvables on which the filter should apply
-	 * @param params: HashMap with parameters
+	 * @param objects Collection of Resolvables on which the filter should apply
+	 * @param params HashMap with parameters
 	 * @return returns a Collection of Resolvables. We suggest to return the modified Collection objects
 	 */
 	public Collection<T> apply(Collection<T> objects, HashMap<String, Object> params);
