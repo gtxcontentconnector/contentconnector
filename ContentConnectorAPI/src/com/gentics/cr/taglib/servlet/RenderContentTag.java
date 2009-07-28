ec2fc3da4ae13e7146237035e7a41ec90f192efe
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-07-28 10:47:00 +0000

    updated RenderContentTag for servlets like RenderContentTag for portlets (@see com.gentics.cr.taglib.portlet.RenderContentTag)

--- ContentConnectorAPI/src/com/gentics/cr/taglib/servlet/RenderContentTag.java
+++ ContentConnectorAPI/src/com/gentics/cr/taglib/servlet/RenderContentTag.java
@@ -2,16 +2,24 @@ package com.gentics.cr.taglib.servlet;
 
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.Hashtable;
 
+import javax.portlet.RenderRequest;
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.TagSupport;
 
+import org.apache.log4j.Logger;
+
 import com.gentics.api.portalnode.connector.PLinkReplacer;
+import com.gentics.cr.CRConfigUtil;
 import com.gentics.cr.CRException;
 import com.gentics.cr.CRResolvableBean;
+import com.gentics.cr.configuration.GenericConfiguration;
 import com.gentics.cr.rendering.ContentRenderer;
+import com.gentics.cr.rendering.contentprocessor.ContentPostProcesser;
 
 /**
  * @author norbert
@@ -21,13 +29,25 @@ public class RenderContentTag extends TagSupport {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -940890400477192009L;
+	private static final long serialVersionUID = -5724484220477278975L;
+	private Logger logger = Logger.getLogger("com.gentics.cr.rendering");
 
 	/**
 	 * Name of the render request attribute for the instance of {@link ContentRenderer}
 	 */
 	public final static String RENDERER_PARAM = "rendercontenttag.renderer";
+	
+	/**
+	 * Name of the config attribute for the instance of {@link GenericConfiguration}
+	 */
+	public final static String CRCONF_PARAM = "rendercontenttag.crconf";
 
+	/**
+	 * Name of the request attribute for the instance of {@link RenderRequest}
+	 */
+	public final static String REQUEST_PARAM = "rendercontenttag.request";
+	
+	
 	/**
 	 * Name of the render request attribute for the instance of {@link PLinkReplacer}
 	 */
@@ -37,6 +57,7 @@ public class RenderContentTag extends TagSupport {
 	 * Rendered object
 	 */
 	protected CRResolvableBean object;
+	public static final String SESSION_KEY_CONTENTPOSTPROCESSOR_CONF = RenderContentTag.class.getName() + "|ContentPostProcessor|confs";
 
 	/**
 	 * name of the rendered attribute
@@ -69,7 +90,7 @@ public class RenderContentTag extends TagSupport {
 	}
 	
 	/**
-	 * Set the content attribute to be rendered
+	 * Set the flag if the returned content should be url-encoded
 	 * @param urlencode 
 	 * 
 	 */
@@ -95,17 +116,36 @@ public class RenderContentTag extends TagSupport {
 	public int doEndTag() throws JspException {
 		// get the ContentRenderer
 		ServletRequest renderRequest = getServletRequest();
+		HttpSession session = pageContext.getSession();
 
 		ContentRenderer renderer = (ContentRenderer)renderRequest.getAttribute(RENDERER_PARAM);
 		PLinkReplacer pLinkReplacer = (PLinkReplacer)renderRequest.getAttribute(PLINK_PARAM);
-
+		CRConfigUtil crConf = (CRConfigUtil)renderRequest.getAttribute(CRCONF_PARAM);
+		
+		
 		try {
 			if (object != null) {
 				try {
 					String content = renderer.renderContent(object, contentAttribute, true, pLinkReplacer, false, null);
+					
+					/* Get the ContentPostProcessor-Config from the PortletSession or instance it from the Config*/
+					@SuppressWarnings("unchecked")
+					Hashtable<String,ContentPostProcesser> confs = (Hashtable<String, ContentPostProcesser>) session.getAttribute(SESSION_KEY_CONTENTPOSTPROCESSOR_CONF);
+					if (confs == null){
+						confs = ContentPostProcesser.getProcessorTable(crConf);
+						session.setAttribute(SESSION_KEY_CONTENTPOSTPROCESSOR_CONF, confs);
+						logger.debug("Put ContentPostProcessor config into session of servlet " + session.getServletContext().getServletContextName() + "!");
+					}
+					if (confs != null) {
+						for(ContentPostProcesser p:confs.values()) {
+							content = p.processString(content, renderRequest);
+						}
+					}
+					
 					if (urlencode) {
 						content = URLEncoder.encode(content, "UTF-8");
 					}
+					
 					if (var != null) {
 						if (content!=null && "".equals(content)) {
 							content = null;
