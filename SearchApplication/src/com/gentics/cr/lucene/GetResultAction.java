64e696b87d39f844def9ba90571e062e4f4c9d0d
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-08-27 13:17:55 +0000

    new version

--- SearchApplication/src/com/gentics/cr/lucene/GetResultAction.java
+++ SearchApplication/src/com/gentics/cr/lucene/GetResultAction.java
@@ -1,6 +1,7 @@
 package com.gentics.cr.lucene;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.apache.log4j.Logger;
 
@@ -15,11 +16,12 @@ import com.gentics.cr.CRResolvableBean;
 import com.gentics.cr.RequestProcessor;
 import com.gentics.cr.StaticRPContainer;
 import com.gentics.cr.configuration.StaticConfigurationContainer;
+import com.gentics.cr.lucene.search.LuceneRequestProcessor;
 
 
 public class GetResultAction extends GenericPluggableAction {
 	
-	Logger logger = Logger.getLogger("GetResultAction");
+	private static final Logger logger = Logger.getLogger(GetResultAction.class);
 
 	public boolean processAction(PluggableActionRequest req, PluggableActionResponse res) throws PluggableActionException {
 		
@@ -33,12 +35,26 @@ public class GetResultAction extends GenericPluggableAction {
 				CRConfigUtil config = StaticConfigurationContainer.getConfig(this.getModule().getModuleId(), null);
 				RequestProcessor rp = StaticRPContainer.getRP(config, 1);
 				long e1 = System.currentTimeMillis();
-				System.out.println("INSTANTIATE TOOK "+(e1-s1)+"ms");
+				logger.debug("INSTANTIATE TOOK "+(e1-s1)+"ms");
+				long s2 = System.currentTimeMillis();
 				CRRequest rq = new CRRequest();
 				rq.setRequestFilter(query);
 				rq.set("secondary", "true");
+				rq.set(LuceneRequestProcessor.META_RESOLVABLE_KEY, true);
 				rq.setAttributeArray(new String[]{"content","binarycontent","editor","name"});
 				Collection<CRResolvableBean> coll = rp.getObjects(rq);
+				if(coll!=null)
+				{
+					Iterator<CRResolvableBean> it = coll.iterator();
+					if(it.hasNext())
+					{
+						CRResolvableBean meta = it.next();
+						it.remove();
+						res.setParameter("hits", meta.get(LuceneRequestProcessor.META_HITS_KEY));
+					}
+				}
+				long e2 = System.currentTimeMillis();
+				logger.debug("Search took "+(e2-s2)+"ms");
 				res.setParameter("result", coll);
 				return true;
 			}
