5bb497606f21f921dd624facbeac1ae248be039a
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-09-10 08:09:19 +0000

    added language guesser

--- ContentConnectorAPI/src/com/gentics/cr/analytics/language/LanguageGuesser.java
+++ ContentConnectorAPI/src/com/gentics/cr/analytics/language/LanguageGuesser.java
@@ -5,10 +5,21 @@ import java.io.InputStream;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.nutch.analysis.lang.LanguageIdentifier;
-
+/**
+ * 
+ * Last changed: $Date: 2009-09-02 17:57:48 +0200 (Mi, 02 Sep 2009) $
+ * @version $Revision: 180 $
+ * @author $Author: supnig@constantinopel.at $
+ *
+ */
 public class LanguageGuesser {
-private static LanguageIdentifier langID = null;
+	private static LanguageIdentifier langID = null;
 	
+	/**
+	 * Detect language from a String
+	 * @param text
+	 * @return detected language
+	 */
 	public static String detectLanguage(String text)
 	{
 		LanguageIdentifier li = getLIInstance();
@@ -16,12 +27,25 @@ private static LanguageIdentifier langID = null;
 		return li.identify(text);
 	}
 	
+	/**
+	 * Detect language from an input stream
+	 * @param is
+	 * @return detected language
+	 * @throws IOException
+	 */
 	public static String detectLanguage(InputStream is) throws IOException
 	{
 		LanguageIdentifier li = getLIInstance();
 		return li.identify(is);
 	}
 	
+	/**
+	 * Detect language from an input stream
+	 * @param is
+	 * @param charset
+	 * @return detected language
+	 * @throws IOException
+	 */
 	public static String detectLanguage(InputStream is, String charset) throws IOException
 	{
 		LanguageIdentifier li = getLIInstance();
