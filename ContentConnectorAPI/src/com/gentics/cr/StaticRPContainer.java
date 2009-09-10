939ee9f7a238f5ddf3b666787c94e3db5b40ef68
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-09-10 08:09:52 +0000

    added javadoc comments

--- ContentConnectorAPI/src/com/gentics/cr/StaticRPContainer.java
+++ ContentConnectorAPI/src/com/gentics/cr/StaticRPContainer.java
@@ -4,7 +4,13 @@ package com.gentics.cr;
 import java.util.Hashtable;
 
 import org.apache.log4j.Logger;
-
+/**
+ * 
+ * Last changed: $Date: 2009-09-02 17:57:48 +0200 (Mi, 02 Sep 2009) $
+ * @version $Revision: 180 $
+ * @author $Author: supnig@constantinopel.at $
+ *
+ */
 public class StaticRPContainer {
 	
 	private static Logger log = Logger.getLogger(StaticRPContainer.class);
@@ -12,9 +18,10 @@ public class StaticRPContainer {
 	
 	/**
 	 * Fetches a already created instance of the request processor. If request processor was not created yet, one will be instantiated.
-	 * @param key
-	 * @param webapproot
-	 * @return
+	 * @param config 
+	 * @param rpnumber 
+	 * @return 
+	 * @throws CRException 
 	 */
 	public static RequestProcessor getRP(CRConfigUtil config, int rpnumber) throws CRException
 	{
