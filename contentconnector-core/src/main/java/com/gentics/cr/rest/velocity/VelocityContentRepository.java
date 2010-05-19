1c926ee87fa694a986e715ef71a51d342af0630e
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-05-19 10:50:41 +0000

    reformated for checkstyle

--- contentconnector-core/src/main/java/com/gentics/cr/rest/velocity/VelocityContentRepository.java
+++ contentconnector-core/src/main/java/com/gentics/cr/rest/velocity/VelocityContentRepository.java
@@ -17,7 +17,8 @@ import com.gentics.cr.template.FileTemplate;
 import com.gentics.cr.template.ITemplate;
 import com.gentics.cr.template.ITemplateManager;
 /**
- * 
+ * VelocityContentRepository allows you to render the result of the
+ * request to the ContentConnnector with velocity.
  * Last changed: $Date: 2010-02-26 17:25:31 +0100 (Fr, 26 Feb 2010) $
  * @version $Revision: 456 $
  * @author $Author: bigbear.ap $
@@ -25,124 +26,180 @@ import com.gentics.cr.template.ITemplateManager;
  */
 public class VelocityContentRepository extends ContentRepository {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -3555742920271252693L;
-	private CRConfigUtil config;
-	private ITemplateManager templateManager;
-	private ITemplate template;
-	private ITemplate errorTemplate;
+  /**
+   * Generated version identifier for serialization.
+   */
+  private static final long serialVersionUID = -3555742920271252693L;
 
-	private boolean templateReloading = false;
-	
-	private static final String TEMPLATEPATH_KEY = "cr.velocity.defaulttemplate";
-	private static final String TEMPLATERELOADING_KEY = "cr.velocity.templatereloading";
-	
-	/**
-	 * Create new Instance of VelocityContentRepository
-	 * @param attr
-	 * @param encoding
-	 * @param options
-	 * @param configUtil
-	 */
-	public VelocityContentRepository(String[] attr, String encoding,
-			String[] options, CRConfigUtil configUtil) {
-		super(attr, encoding, options);
-		config = configUtil;
-		templateReloading = Boolean.parseBoolean((String) config.get(TEMPLATERELOADING_KEY));
-	}
+  /**
+   * CRConfig to use by the instance.
+   */
+  private CRConfigUtil config;
 
-	private Logger logger = Logger.getLogger(VelocityContentRepository.class);
-	
-	@Override
-	public void respondWithError(OutputStream stream, CRException ex,
-			boolean isDebug) {
-		logger.error("Error getting result.",ex);
-		ensureTemplateManager();
-		try {
-			loadTemplate(true);
-			templateManager.put("exception",ex);
-			templateManager.put("debug",isDebug);
-			String encoding = this.getResponseEncoding();
-			templateManager.put("encoding",encoding);
-			//TODO use errorTemplateName (absolute) instead of errorTemplate.getKey()
-			String output = templateManager.render(errorTemplate.getKey(), errorTemplate.getSource());
-			stream.write(output.getBytes(encoding));
-		} catch (Exception e){
-			logger.error("Cannot succesfully respond with error template.",e);
-		}
-		
-	
-	}
+  /**
+   * Template manager for velocity.
+   */
+  private ITemplateManager templateManager;
 
-	@Override
-	public void toStream(OutputStream stream) throws CRException {
-		try {
-			ensureTemplateManager();
-			loadTemplate();
-			templateManager.put("resolvables", this.resolvableColl);
-			String encoding = this.getResponseEncoding();
-			templateManager.put("encoding",encoding);
-			//TODO use templateName (absolute) instead of errorTemplate.getKey()
-			String output = templateManager.render(template.getKey(), template.getSource());
-			stream.write(output.getBytes(encoding));
-		} catch (CRException e) {
-			respondWithError(stream, e, false);
-		} catch (IOException e) {
-			logger.error("Cannot write to Output stream.",e);
-		}
-	}
-	
-	private void ensureTemplateManager(){
-		if(templateManager==null){
-			templateManager=config.getTemplateManager();
-			templateManager.put("esc", new EscapeTool());
-		}
-	}
-	
-	private void loadTemplate() throws CRException{
-		loadTemplate(false);
-	}
-	
-	private void loadTemplate(boolean loadErrorTemplate) throws CRException{
-		if((template==null && !loadErrorTemplate) || (errorTemplate==null && loadErrorTemplate) || templateReloading){
-			String templatePath = (String) config.get(TEMPLATEPATH_KEY);
-			try{
-				if(loadErrorTemplate) {
-					//make velocity.vm to velocity.error.vm
-					File errorTemplateFile = new File(templatePath);
-					String directoryName = errorTemplateFile.getParent();
-					String fileName = errorTemplateFile.getName();
-					String fileExtension = fileName.replaceAll(".*\\.", "");
-					fileName = fileName.replaceAll("(.*)\\..*", "$1");
-					templatePath = fileName + ".error."+fileExtension;
-					if(directoryName!=null) templatePath = directoryName + File.separator + templatePath;
-					errorTemplate = getFileTemplate(templatePath);
-				} else {
-					template = getFileTemplate(templatePath);
-				}
-				
-			}
-			catch(Exception e)
-			{
-				log.error("FAILED TO LOAD VELOCITY TEMPLATE FROM "+template,e);
-			}
-		}
-		if(template==null && !loadErrorTemplate){
-			throw new CRException(new CRError("ERROR","The template "+template+" cannot be found."));
-		}
-		if(errorTemplate==null && loadErrorTemplate){
-			throw new CRException(new CRError("ERROR","The template "+template+" cannot be found."));
-		}
-	}
-	
-	private FileTemplate getFileTemplate(String templatePath) throws FileNotFoundException, CRException{
-		File file = new File(templatePath);
-		if(!file.isAbsolute()){
-			file= new File(CRConfigUtil.DEFAULT_TEMPLATE_PATH+File.separator+templatePath);
-		}
-		return new FileTemplate(new FileInputStream(file));
-	}
-	
+  /**
+   * Velocity Template to use for rendering a sucessful response.
+   */
+  private ITemplate template;
+
+  /**
+   * Velocity Template used for rendering the error response.
+   */
+  private ITemplate errorTemplate;
+
+  /**
+   * When true templates are loaded from file system with every request.
+   * Otherwise the will be cached between the requests.
+   */
+  private boolean templateReloading = false;
+
+  /**
+   * Key for the configuration of the template in the config.
+   */
+  private static final String TEMPLATEPATH_KEY = "cr.velocity.defaulttemplate";
+
+  /**
+   * Key for the configuration of the error template in the config.
+   */
+  private static final String TEMPLATERELOADING_KEY =
+    "cr.velocity.templatereloading";
+
+  /**
+   * Log4j logger to log errors and debug.
+   */
+  private Logger logger = Logger.getLogger(VelocityContentRepository.class);
+
+  /**
+   * Create new Instance of VelocityContentRepository.
+   * @param attr TODO javadoc
+   * @param encoding TODO javadoc
+   * @param options TODO javadoc
+   * @param configUtil TODO javadoc
+   */
+  public VelocityContentRepository(final String[] attr, final String encoding,
+      final String[] options, final CRConfigUtil configUtil) {
+    super(attr, encoding, options);
+    config = configUtil;
+    templateReloading = Boolean.parseBoolean(
+        (String) config.get(TEMPLATERELOADING_KEY));
+  }
+
+  @Override
+  public final void respondWithError(final OutputStream stream,
+      final CRException ex, final boolean isDebug) {
+    logger.error("Error getting result.", ex);
+    ensureTemplateManager();
+    try {
+      loadTemplate(true);
+      templateManager.put("exception", ex);
+      templateManager.put("debug", isDebug);
+      String encoding = this.getResponseEncoding();
+      templateManager.put("encoding", encoding);
+      //TODO use errorTemplateName (absolute) instead of errorTemplate.getKey()
+      String output = templateManager.render(errorTemplate.getKey(),
+          errorTemplate.getSource());
+      stream.write(output.getBytes(encoding));
+    } catch (Exception e) {
+      logger.error("Cannot succesfully respond with error template.", e);
+    }
+  }
+
+  @Override
+  public final void toStream(final OutputStream stream) throws CRException {
+    try {
+      ensureTemplateManager();
+      loadTemplate();
+      templateManager.put("resolvables", this.resolvableColl);
+      String encoding = this.getResponseEncoding();
+      templateManager.put("encoding", encoding);
+      //TODO use templateName (absolute) instead of errorTemplate.getKey()
+      String output = templateManager.render(template.getKey(),
+          template.getSource());
+      stream.write(output.getBytes(encoding));
+    } catch (CRException e) {
+      respondWithError(stream, e, false);
+    } catch (IOException e) {
+      logger.error("Cannot write to Output stream.", e);
+    }
+  }
+
+  /**
+   * TODO javadoc.
+   */
+  private void ensureTemplateManager() {
+    if (templateManager == null) {
+      templateManager = config.getTemplateManager();
+      templateManager.put("esc", new EscapeTool());
+    }
+  }
+
+  /**
+   * TODO javadoc.
+   * @throws CRException TODO javadoc
+   */
+  private void loadTemplate() throws CRException {
+    loadTemplate(false);
+  }
+
+  /**
+   * TODO javadoc.
+   * @param loadErrorTemplate TODO javadoc
+   * @throws CRException TODO javadoc
+   */
+  private void loadTemplate(final boolean loadErrorTemplate) throws CRException
+  {
+    if ((template == null && !loadErrorTemplate)
+        || (errorTemplate == null && loadErrorTemplate) || templateReloading) {
+      String templatePath = (String) config.get(TEMPLATEPATH_KEY);
+      try {
+        if (loadErrorTemplate) {
+          //make velocity.vm to velocity.error.vm
+          File errorTemplateFile = new File(templatePath);
+          String directoryName = errorTemplateFile.getParent();
+          String fileName = errorTemplateFile.getName();
+          String fileExtension = fileName.replaceAll(".*\\.", "");
+          fileName = fileName.replaceAll("(.*)\\..*", "$1");
+          templatePath = fileName + ".error." + fileExtension;
+          if (directoryName != null) {
+            templatePath = directoryName + File.separator + templatePath;
+          }
+          errorTemplate = getFileTemplate(templatePath);
+        } else {
+          template = getFileTemplate(templatePath);
+        }
+      } catch (Exception e) {
+        log.error("FAILED TO LOAD VELOCITY TEMPLATE FROM " + template, e);
+      }
+    }
+    if (template == null && !loadErrorTemplate) {
+      throw new CRException(new CRError("ERROR",
+          "The template " + template + " cannot be found."));
+    }
+    if (errorTemplate == null && loadErrorTemplate) {
+      throw new CRException(new CRError("ERROR",
+          "The template " + template + " cannot be found."));
+    }
+  }
+
+  /**
+   * TODO javadoc.
+   * @param templatePath TODO javadoc
+   * @return TODO javadoc
+   * @throws FileNotFoundException TODO javadoc
+   * @throws CRException TODO javadoc
+   */
+  private FileTemplate getFileTemplate(final String templatePath)
+      throws FileNotFoundException, CRException {
+    File file = new File(templatePath);
+    if (!file.isAbsolute()) {
+      file = new File(CRConfigUtil.DEFAULT_TEMPLATE_PATH + File.separator
+          + templatePath);
+    }
+    return new FileTemplate(new FileInputStream(file));
+  }
+
 }
