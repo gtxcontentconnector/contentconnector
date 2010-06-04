e6db50807d42f4ada50021284fa526231f23763f
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-06-04 12:44:44 +0000

    added loggers for tracing open datasource handles. reformated for checkstyle compliance

--- contentconnector-core/src/main/java/com/gentics/cr/CRDatabaseFactory.java
+++ contentconnector-core/src/main/java/com/gentics/cr/CRDatabaseFactory.java
@@ -15,98 +15,90 @@ import com.gentics.cr.portalnode.PortalNodeInteractor;
  *
  */
 public class CRDatabaseFactory {
-	//Static Members
-	private static Logger log = Logger.getLogger(CRDatabaseFactory.class);
-	private static CRDatabaseFactory instance;
-	
-	//Instance Members
-	private long dbcount = 0;
-	
-	private static CRDatabaseFactory getInstance()
-	{
-		if(instance==null)instance=new CRDatabaseFactory();
-		return instance;
-	}
-	
-	/**
-	 * Release Datasource instance
-	 * @param ds
-	 */
-	public static void releaseDatasource(Datasource ds)
-	{
-		if(ds!=null)
-		{
-			getInstance().releaseDS();
-			ds = null;
-		}
-	}
-	
-	private synchronized void releaseDS()
-	{
-		dbcount--;
-	}
-	
-	private synchronized void accquireDS()
-	{
-		dbcount++;
-	}
-	
-	private synchronized boolean destroyFactory()
-	{
-		if(dbcount<=0)
-		{
-			PortalConnectorFactory.destroy();
-			log.debug("Factory, resources and threads have been closed.");
-			return true;
-		}
-		log.error("There are still unreleased datasources => could not destroy the factory");
-		return false;
-	}
-	
-	/**
-	 * Destroys the Factory and releases all resources and stops threads if there are no more datasources that were not released
-	 * @return true if there were no unreleased datasources and the factory was destroyed
-	 */
-	public static boolean destroy()
-	{
-		return getInstance().destroyFactory();
-	}
-	
-	/**
-	 * Gets a Datasource instance that ins configured within the given requestProcessorConfig
-	 * @param requestProcessorConfig containing the datasource config
-	 * @return Datasource if correctly configured, otherwise null
-	 */
-	public static Datasource getDatasource(CRConfigUtil requestProcessorConfig)
-	{
-		Datasource ds = null;
-		Properties ds_handle = requestProcessorConfig.getDatasourceHandleProperties();
-		Properties ds_props = requestProcessorConfig.getDatasourceProperties();
-		if(ds_handle!=null && ds_handle.size()!=0)
-		{
-			if(ds_handle.containsKey("portalnodedb"))
-			{
-				String key = (String)ds_handle.get("portalnodedb");
-				ds = PortalNodeInteractor.getPortalnodeDatasource(key);
-			}
-			else if(ds_props!=null && ds_props.size()!=0)
-			{
-				ds = PortalConnectorFactory.createWriteableDatasource(ds_handle,ds_props);
-			}
-			else
-			{
-				ds = PortalConnectorFactory.createWriteableDatasource(ds_handle);
-			}	
-			log.debug("Datasource created for "+requestProcessorConfig.getName());
-			if(ds!=null)
-			{
-				getInstance().accquireDS();
-			}
-		}
-		else
-		{
-			log.debug("No Datasource created for"+requestProcessorConfig.getName());
-		}
-		return(ds);
-	}
+  //Static Members
+  private static Logger log = Logger.getLogger(CRDatabaseFactory.class);
+  private static CRDatabaseFactory instance;
+  
+  //Instance Members
+  private long dbcount = 0;
+  
+  private static CRDatabaseFactory getInstance() {
+    if(instance==null)instance=new CRDatabaseFactory();
+    return instance;
+  }
+
+  /**
+   * Release Datasource instance.
+   * @param ds
+   */
+  public static void releaseDatasource(Datasource ds) {
+    log.debug("Release Datasource" + ds);
+    if (ds != null) {
+      getInstance().releaseDS();
+      ds = null;
+    }
+  }
+
+  private synchronized void releaseDS() {
+    dbcount--;
+    log.debug("Released DB, DBCount now: " + dbcount);
+  }
+
+  private synchronized void accquireDS() {
+    dbcount++;
+    log.debug("Added DB, DBCount now: " + dbcount);
+  }
+
+  private synchronized boolean destroyFactory() {
+    if(dbcount<=0)
+    {
+      PortalConnectorFactory.destroy();
+      log.debug("Factory, resources and threads have been closed.");
+      return true;
+    }
+    log.error("There are still unreleased datasources => could not destroy the factory");
+    return false;
+  }
+  
+  /**
+   * Destroys the Factory and releases all resources and stops threads if there are no more datasources that were not released
+   * @return true if there were no unreleased datasources and the factory was destroyed
+   */
+  public static boolean destroy() {
+    return getInstance().destroyFactory();
+  }
+
+  /**
+   * Gets a Datasource instance that ins configured within the given
+   * requestProcessorConfig.
+   * @param requestProcessorConfig containing the datasource config
+   * @return Datasource if correctly configured, otherwise null
+   */
+  public static Datasource getDatasource(
+      final CRConfigUtil requestProcessorConfig) {
+    Datasource ds = null;
+    Properties dsHandle =
+      requestProcessorConfig.getDatasourceHandleProperties();
+    Properties dsProps = requestProcessorConfig.getDatasourceProperties();
+    if (dsHandle != null && dsHandle.size() != 0) {
+      if (dsHandle.containsKey("portalnodedb")) {
+        String key = (String) dsHandle.get("portalnodedb");
+        ds = PortalNodeInteractor.getPortalnodeDatasource(key);
+      } else if (dsProps != null && dsProps.size() != 0) {
+        ds =
+          PortalConnectorFactory.createWriteableDatasource(dsHandle, dsProps);
+      } else {
+        ds = PortalConnectorFactory.createWriteableDatasource(dsHandle);
+      }
+      log.debug("Datasource created for " + requestProcessorConfig.getName());
+      if (ds != null) {
+        getInstance().accquireDS();
+      }
+    }
+    else
+    {
+      log.debug("No Datasource created for " + requestProcessorConfig.getName());
+    }
+    return(ds);
+  }
 }
