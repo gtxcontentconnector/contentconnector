a1fb2a293ff0b15d5109a78f25d1b6f80ed8b941
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-06-07 09:11:47 +0000

    reformated for more compatiblity with checkstyle

--- contentconnector-lucene/src/main/java/com/gentics/cr/lucene/IndexJobServlet.java
+++ contentconnector-lucene/src/main/java/com/gentics/cr/lucene/IndexJobServlet.java
@@ -27,176 +27,175 @@ import com.gentics.cr.util.indexing.IndexLocation;
  */
 public class IndexJobServlet extends HttpServlet {
 
-	private static final String NAGIOS_PARAM = "nagios";
-	private static final long serialVersionUID = 0002L;
-	private Logger log;
+  private static final String NAGIOS_PARAM = "nagios";
+  private static final long serialVersionUID = 0002L;
+  private Logger log;
 
-	private IndexController indexer;
-
-
-
-	public final void init(final ServletConfig config) throws ServletException {
-
-		super.init(config);
-		this.log = Logger.getLogger("com.gentics.cr.lucene");
-		this.indexer = new IndexController(config.getServletName());
-		
-	}
-
-	@Override
-	public final void destroy() {
-		if (indexer != null) {
-			indexer.stop();
-		}
-	}
-
-	/**
-	 * Wrapper Method for the doGet and doPost Methods
-	 * 
-	 * @param request
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	public void doService(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
-		this.log.debug("Request:" + request.getQueryString());
-		String nagString = request.getParameter(NAGIOS_PARAM);
-		boolean doNag = Boolean.parseBoolean(nagString);
-		// starttime
-		long s = new Date().getTime();
-		// get the objects
-
-		String action = request.getParameter("action");
-		String index = request.getParameter("idx");
-
-		if(doNag)
-		{
-			response.setContentType("text/plain");
-			Hashtable<String,IndexLocation> indexTable = indexer.getIndexes();
-			for (Entry<String,IndexLocation> e:indexTable.entrySet()) {
-
-				if(e.getKey().equalsIgnoreCase(index))
-				{
-					IndexLocation loc = e.getValue();
-					IndexJobQueue queue = loc.getQueue();
-					if(queue!=null && queue.isRunning())
-					{
-						response.getWriter().write("WorkerThread:OK\n");
-					}
-					else
-					{
-						response.getWriter().write("WorkerThread:NOK\n");
-					}
-					response.getWriter().write("ObjectsInIndex:"+loc.getDocCount()+"\n");
-					AbstractUpdateCheckerJob j = queue.getCurrentJob();
-					if(j!=null)
-					{
-						response.getWriter().write("CurrentJobObjectsToIndex:"+j.getObjectsToIndex()+"\n");
-					}
+  private IndexController indexer;
 
 
 
-				}
-			}
-		} else {
-			String nc = "&t="+System.currentTimeMillis();
+  public final void init(final ServletConfig config) throws ServletException {
 
-			response.setContentType("text/html");
-			response.getWriter().write("<html>\r\n"+
-					"<head>\r\n" +
-					"<title>IndexerServlet for Lucene by Gentics</title>\r\n" +
-					"<meta http-equiv=\"refresh\" content=\"5; URL=\"?ts="+(new Date().getTime())+"\" />" +
-					"</head>\r\n" +
-					"<body>\r\n");
-			Hashtable<String,IndexLocation> indexTable = indexer.getIndexes();
-			for (Entry<String,IndexLocation> e:indexTable.entrySet()) {
-				
-				response.getWriter().write("<h1>Index "+e.getKey()+"</h1><br/>\n");
-				IndexLocation loc = e.getValue();
-				IndexJobQueue queue = loc.getQueue();
-				
-				Hashtable<String,CRConfigUtil> map = loc.getCRMap();
-				
-				if(e.getKey().equalsIgnoreCase(index))
-				{
-					if("stopWorker".equalsIgnoreCase(action))
-						queue.stopWorker();
-					if("startWorker".equalsIgnoreCase(action))
-						queue.startWorker();
-					if("addJob".equalsIgnoreCase(action))
-					{
-						String cr = request.getParameter("cr");
-						if("all".equalsIgnoreCase(cr))
-							loc.createAllCRIndexJobs();
-						else
-						{
-							if(cr!=null)
-							{
-								CRConfigUtil crc=map.get(cr);
-								loc.createCRIndexJob(crc,map);
-							}
-						}
-					}
-				}
-				response.getWriter().write("ObjectsInIndex: "+loc.getDocCount()+"<br/>\n");
-				ArrayList<AbstractUpdateCheckerJob> lastJobs = queue.getLastJobs();
-				for(AbstractUpdateCheckerJob lj:lastJobs)
-				{
-					response.getWriter().write("Job "+lj.getIdentifyer()+" took "+lj.getDuration()+"ms for "+lj.getObjectsDone()+" objects<br/>\n");
-				}
-				
-				response.getWriter().write(queue.getSize()+" Elements in queue<br/>\n");
-				response.getWriter().write("Current interval: "+queue.getInterval()+"s<br/>\n");
-				if(loc.isPeriodical())
-				{
-					response.getWriter().write("All CRs will be indexed periodical<br/>\n");
-				}
-				if(queue.isRunning())
-				{
-					response.getWriter().write("Worker is processing queue (<a href=\"?action=stopWorker&idx="+e.getKey()+nc+"\">stop</a>)<br/>\n");
-				}
-				else
-				{
-					response.getWriter().write("Worker not running (<a href=\"?action=startWorker&idx="+e.getKey()+nc+"\">start</a>)<br/>\n");
-				}
-				AbstractUpdateCheckerJob j = queue.getCurrentJob();
+    super.init(config);
+    this.log = Logger.getLogger("com.gentics.cr.lucene");
+    this.indexer = new IndexController(config.getServletName());
 
-				if (j != null) {
-					response.getWriter().write("<hr><br/>\n");
-					response.getWriter().write("Current Job "+j.getIdentifyer()+"<br/>\n");
-					response.getWriter().write("Current Status: "+j.getStatusString()+"<br/>\n");
-					response.getWriter().write("Objects to index "+j.getObjectsToIndex()+"<br/>\n");
-					response.getWriter().write("Objects done "+j.getObjectsDone()+"<br/>\n");
-					response.getWriter().write("<hr><br/>\n");
-				}
-				for (Entry<String,CRConfigUtil> crc:map.entrySet()) {
-					response.getWriter().write("Add CR "+crc.getKey()+" for indexing (<a href=\"?action=addJob&idx="+e.getKey()+"&cr="+crc.getKey()+nc+"\">add</a>)<br/>\n");
-				}
-				response.getWriter().write("Add all CRs for indexing (<a href=\"?action=addJob&idx="+e.getKey()+"&cr=all"+nc+"\">add</a>)<br/>\n");
-				
-			}
-			
-			response.getWriter().write("<a href=\"?action=show"+nc+"\">Refresh status</a><br/>\n");
-			response.getWriter().write("</body></html>");
-		}
-		response.getWriter().flush();
-		response.getWriter().close();
-		// endtime
-		long e = new Date().getTime();
-		this.log.info("Executiontime for getting Status " + (e - s));
+  }
 
-	}
+  @Override
+  public final void destroy() {
+    if (indexer != null) {
+      indexer.stop();
+    }
+  }
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doService(request, response);
-	}
+  /**
+   * Wrapper Method for the doGet and doPost Methods
+   * 
+   * @param request
+   * @param response
+   * @throws ServletException
+   * @throws IOException
+   */
+  public void doService(HttpServletRequest request,
+      HttpServletResponse response) throws ServletException, IOException {
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doService(request, response);
-	}
+    this.log.debug("Request:" + request.getQueryString());
+    String nagString = request.getParameter(NAGIOS_PARAM);
+    boolean doNag = Boolean.parseBoolean(nagString);
+    // starttime
+    long s = new Date().getTime();
+    // get the objects
+
+    String action = request.getParameter("action");
+    String index = request.getParameter("idx");
+
+    if (doNag) {
+      response.setContentType("text/plain");
+      Hashtable<String, IndexLocation> indexTable = indexer.getIndexes();
+      for (Entry<String, IndexLocation> e : indexTable.entrySet()) {
+
+        if(e.getKey().equalsIgnoreCase(index))
+        {
+          IndexLocation loc = e.getValue();
+          IndexJobQueue queue = loc.getQueue();
+          if(queue!=null && queue.isRunning())
+          {
+            response.getWriter().write("WorkerThread:OK\n");
+          }
+          else
+          {
+            response.getWriter().write("WorkerThread:NOK\n");
+          }
+          response.getWriter().write("ObjectsInIndex:"+loc.getDocCount()+"\n");
+          AbstractUpdateCheckerJob j = queue.getCurrentJob();
+          if(j!=null)
+          {
+            response.getWriter().write("CurrentJobObjectsToIndex:"+j.getObjectsToIndex()+"\n");
+          }
+
+
+
+        }
+      }
+    } else {
+      String nc = "&t="+System.currentTimeMillis();
+
+      response.setContentType("text/html");
+      response.getWriter().write("<html>\r\n"+
+          "<head>\r\n" +
+          "<title>IndexerServlet for Lucene by Gentics</title>\r\n" +
+          "<meta http-equiv=\"refresh\" content=\"5; URL=\"?ts="+(new Date().getTime())+"\" />" +
+          "</head>\r\n" +
+          "<body>\r\n");
+      Hashtable<String,IndexLocation> indexTable = indexer.getIndexes();
+      for (Entry<String,IndexLocation> e:indexTable.entrySet()) {
+        
+        response.getWriter().write("<h1>Index "+e.getKey()+"</h1><br/>\n");
+        IndexLocation loc = e.getValue();
+        IndexJobQueue queue = loc.getQueue();
+        
+        Hashtable<String,CRConfigUtil> map = loc.getCRMap();
+        
+        if(e.getKey().equalsIgnoreCase(index))
+        {
+          if("stopWorker".equalsIgnoreCase(action))
+            queue.stopWorker();
+          if("startWorker".equalsIgnoreCase(action))
+            queue.startWorker();
+          if("addJob".equalsIgnoreCase(action))
+          {
+            String cr = request.getParameter("cr");
+            if("all".equalsIgnoreCase(cr))
+              loc.createAllCRIndexJobs();
+            else
+            {
+              if(cr!=null)
+              {
+                CRConfigUtil crc=map.get(cr);
+                loc.createCRIndexJob(crc,map);
+              }
+            }
+          }
+        }
+        response.getWriter().write("ObjectsInIndex: "+loc.getDocCount()+"<br/>\n");
+        ArrayList<AbstractUpdateCheckerJob> lastJobs = queue.getLastJobs();
+        for(AbstractUpdateCheckerJob lj:lastJobs)
+        {
+          response.getWriter().write("Job "+lj.getIdentifyer()+" took "+lj.getDuration()+"ms for "+lj.getObjectsDone()+" objects<br/>\n");
+        }
+        
+        response.getWriter().write(queue.getSize()+" Elements in queue<br/>\n");
+        response.getWriter().write("Current interval: "+queue.getInterval()+"s<br/>\n");
+        if(loc.isPeriodical())
+        {
+          response.getWriter().write("All CRs will be indexed periodical<br/>\n");
+        }
+        if(queue.isRunning())
+        {
+          response.getWriter().write("Worker is processing queue (<a href=\"?action=stopWorker&idx="+e.getKey()+nc+"\">stop</a>)<br/>\n");
+        }
+        else
+        {
+          response.getWriter().write("Worker not running (<a href=\"?action=startWorker&idx="+e.getKey()+nc+"\">start</a>)<br/>\n");
+        }
+        AbstractUpdateCheckerJob j = queue.getCurrentJob();
+
+        if (j != null) {
+          response.getWriter().write("<hr><br/>\n");
+          response.getWriter().write("Current Job "+j.getIdentifyer()+"<br/>\n");
+          response.getWriter().write("Current Status: "+j.getStatusString()+"<br/>\n");
+          response.getWriter().write("Objects to index "+j.getObjectsToIndex()+"<br/>\n");
+          response.getWriter().write("Objects done "+j.getObjectsDone()+"<br/>\n");
+          response.getWriter().write("<hr><br/>\n");
+        }
+        for (Entry<String,CRConfigUtil> crc:map.entrySet()) {
+          response.getWriter().write("Add CR "+crc.getKey()+" for indexing (<a href=\"?action=addJob&idx="+e.getKey()+"&cr="+crc.getKey()+nc+"\">add</a>)<br/>\n");
+        }
+        response.getWriter().write("Add all CRs for indexing (<a href=\"?action=addJob&idx="+e.getKey()+"&cr=all"+nc+"\">add</a>)<br/>\n");
+        
+      }
+      
+      response.getWriter().write("<a href=\"?action=show"+nc+"\">Refresh status</a><br/>\n");
+      response.getWriter().write("</body></html>");
+    }
+    response.getWriter().flush();
+    response.getWriter().close();
+    // endtime
+    long e = new Date().getTime();
+    this.log.info("Executiontime for getting Status " + (e - s));
+
+  }
+
+  public void doGet(final HttpServletRequest request,
+      final HttpServletResponse response) throws ServletException, IOException {
+    doService(request, response);
+  }
+
+  public void doPost(final HttpServletRequest request,
+      final HttpServletResponse response) throws ServletException, IOException {
+    doService(request, response);
+  }
 
 }
