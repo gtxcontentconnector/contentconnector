524194376f0084f4215d63b52ddd7237a50faa4c
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-07-22 11:22:19 +0000

    added simplerenderer

---
+++ contentconnector-core/src/main/java/com/gentics/cr/rendering/SimpleRenderer.java
@@ -0,0 +1,78 @@
+package com.gentics.cr.rendering;
+
+import com.gentics.api.portalnode.connector.PLinkReplacer;
+import com.gentics.api.portalnode.connector.PortalConnectorHelper;
+import com.gentics.cr.CRConfig;
+import com.gentics.cr.exceptions.CRException;
+import com.gentics.cr.template.ITemplateManager;
+
+/**
+ * This simple renderer is intended to be put into a velocity context to render special attributes of a bean
+ * 
+ * Last changed: $Date: 2010-04-01 15:25:54 +0200 (Do, 01 Apr 2010) $
+ * @version $Revision: 545 $
+ * @author $Author: supnig@constantinopel.at $
+ *
+ */
+public class SimpleRenderer {
+
+	private boolean doPlinks = true;
+	private boolean doVelocity = true;
+	
+	private CRConfig conf;
+	private PLinkReplacer pr;
+	
+	/**
+	 * Set the SimpleRenderer to enable or disable Velocity
+	 * Default: Velocity enabled
+	 * @param dovtl
+	 */
+	public void setDoVelocity(boolean dovtl)
+	{
+		this.doVelocity = dovtl;
+	}
+	
+	
+	/**
+	 * Set the SimpleRenderer to enable or disable Plinks
+	 * @param doplinks
+	 */
+	public void setDoPlinks(boolean doplinks)
+	{
+		this.doPlinks = doplinks;
+	}
+	
+	/**
+	 * Create a new instance of SimpleRenderer
+	 * @param cr
+	 * @param pr
+	 */
+	public SimpleRenderer(CRConfig conf, PLinkReplacer pr)
+	{
+		this.conf = conf;
+		this.pr = pr;
+	}
+	
+	/**
+	 * Performs velocity and plink rendering on a given string
+	 * @param source
+	 * @return
+	 */
+	public String render(String source)throws CRException
+	{
+		// replace plinks (if configured to do so)
+		if (this.doPlinks && this.pr != null) {
+			source = PortalConnectorHelper.replacePLinks(source,
+					this.pr);
+		}
+
+		if (this.doVelocity) {
+			
+			// Initialize Velocity Context
+			ITemplateManager myTemplateManager = conf.getTemplateManager();
+
+			source = myTemplateManager.render("attribute", source);
+		}
+		return source;
+	}
+}
