aae37fc02e9466ea5c6c46112763ac9288c84663
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-07-22 09:06:53 +0000

    added detailed monitoring to phrase bolder

--- contentconnector-lucene/src/main/java/com/gentics/cr/lucene/search/highlight/PhraseBolder.java
+++ contentconnector-lucene/src/main/java/com/gentics/cr/lucene/search/highlight/PhraseBolder.java
@@ -15,6 +15,8 @@ import org.apache.lucene.search.highlight.TokenGroup;
 
 import com.gentics.cr.configuration.GenericConfiguration;
 import com.gentics.cr.lucene.indexer.index.LuceneAnalyzerFactory;
+import com.gentics.cr.monitoring.MonitorFactory;
+import com.gentics.cr.monitoring.UseCase;
 /**
  * 
  * Last changed: $Date: 2009-06-26 15:48:16 +0200 (Fr, 26 Jun 2009) $
@@ -22,119 +24,123 @@ import com.gentics.cr.lucene.indexer.index.LuceneAnalyzerFactory;
  * @author $Author: supnig@constantinopel.at $
  *
  */
-public class PhraseBolder extends ContentHighlighter implements Formatter{
+public class PhraseBolder extends ContentHighlighter implements Formatter {
 
-	private Analyzer analyzer=null;
-	private int numMaxFragments=3;
-	private int fragmentSize=100;
-	
-	private String highlightPrefix="";
-	private String highlightPostfix="";
-	private String fragmentSeperator="";
-	
-	private static final String NUM_MAX_FRAGMENTS_KEY = "fragments";
-	private static final String NUM_FRAGMENT_SIZE_KEY = "fragmentsize";
-	private static final String PHRASE_PREFIX_KEY = "highlightprefix";
-	private static final String PHRASE_POSTFIX_KEY = "highlightpostfix";
-	private static final String FRAGMENT_SEPERATOR_KEY = "fragmentseperator";
-	/**
-	 * Create new Instance of PhraseBolder
-	 * @param config
-	 */
-	public PhraseBolder(GenericConfiguration config) {
-		super(config);
-		analyzer = LuceneAnalyzerFactory.createAnalyzer(config);
-		
-		highlightPrefix = (String)config.get(PHRASE_PREFIX_KEY);
-		if(highlightPrefix==null)highlightPrefix="<b>";
-		highlightPostfix = (String)config.get(PHRASE_POSTFIX_KEY);
-		if(highlightPostfix==null)highlightPostfix="</b>";
-		fragmentSeperator = (String)config.get(FRAGMENT_SEPERATOR_KEY);
-		if(fragmentSeperator==null)fragmentSeperator="...";
-		
-		String nmF = (String)config.get(NUM_MAX_FRAGMENTS_KEY);
-		if(nmF!=null)
-		{
-			try
-			{
-				int i = Integer.parseInt(nmF);
-				numMaxFragments = i;
-			}catch(NumberFormatException e)
-			{
-				log.error("The configured count of fragments for this ContentHighlighter is not a number");
-			}
-		}
-		String nFS = (String)config.get(NUM_FRAGMENT_SIZE_KEY);
-		if(nFS!=null)
-		{
-			try
-			{
-				int i = Integer.parseInt(nFS);
-				fragmentSize = i;
-			}catch(NumberFormatException e)
-			{
-				log.error("The configured size for fragments for this ContentHighlighter is not a number");
-			}
-		}
-	}
+  private Analyzer analyzer=null;
+  private int numMaxFragments=3;
+  private int fragmentSize=100;
 
-	/**
-	 * Highlights Terms by enclosing them with &lt;b&gt;term&lt;/b&gt;
-	 * @param originalTermText 
-	 * @param tokenGroup 
-	 * @return 
-	 */
-	public String highlightTerm(String originalTermText, TokenGroup tokenGroup) {
-		
-		if (tokenGroup.getTotalScore() <= 0) 
-	      return originalTermText;
-	  
-	    return highlightPrefix + originalTermText + highlightPostfix;
+  private String highlightPrefix="";
+  private String highlightPostfix="";
+  private String fragmentSeperator="";
 
-	}
+  private static final String NUM_MAX_FRAGMENTS_KEY = "fragments";
+  private static final String NUM_FRAGMENT_SIZE_KEY = "fragmentsize";
+  private static final String PHRASE_PREFIX_KEY = "highlightprefix";
+  private static final String PHRASE_POSTFIX_KEY = "highlightpostfix";
+  private static final String FRAGMENT_SEPERATOR_KEY = "fragmentseperator";
+  /**
+   * Create new Instance of PhraseBolder
+   * @param config
+   */
+  public PhraseBolder(GenericConfiguration config) {
+    super(config);
+    analyzer = LuceneAnalyzerFactory.createAnalyzer(config);
+    
+    highlightPrefix = (String)config.get(PHRASE_PREFIX_KEY);
+    if(highlightPrefix==null)highlightPrefix="<b>";
+    highlightPostfix = (String)config.get(PHRASE_POSTFIX_KEY);
+    if(highlightPostfix==null)highlightPostfix="</b>";
+    fragmentSeperator = (String)config.get(FRAGMENT_SEPERATOR_KEY);
+    if(fragmentSeperator==null)fragmentSeperator="...";
+    
+    String nmF = (String)config.get(NUM_MAX_FRAGMENTS_KEY);
+    if(nmF!=null)
+    {
+      try
+      {
+        int i = Integer.parseInt(nmF);
+        numMaxFragments = i;
+      }catch(NumberFormatException e)
+      {
+        log.error("The configured count of fragments for this ContentHighlighter is not a number");
+      }
+    }
+    String nFS = (String)config.get(NUM_FRAGMENT_SIZE_KEY);
+    if(nFS!=null)
+    {
+      try
+      {
+        int i = Integer.parseInt(nFS);
+        fragmentSize = i;
+      }catch(NumberFormatException e)
+      {
+        log.error("The configured size for fragments for this ContentHighlighter is not a number");
+      }
+    }
+  }
 
-	
-	/**
-	 * @param attribute 
-	 * @param parsedQuery 
-	 * @return 
-	 * 
-	 */
-	public String highlight(String attribute, Query parsedQuery) {
-		String result="";
-		if(attribute!=null && parsedQuery!=null)
-		{
-			Highlighter highlighter = new Highlighter(this,new QueryScorer(parsedQuery));
-			highlighter.setTextFragmenter(new WordCountFragmenter(fragmentSize));
-			
-			TokenStream tokenStream = analyzer.tokenStream(this.getHighlightAttribute(), new StringReader(attribute));
-	
-			
-			try {
-				TextFragment[] frags = highlighter.getBestTextFragments(tokenStream, attribute, true, numMaxFragments);
-				boolean first=true;
-				for(TextFragment frag:frags)
-				{
-					if(!first)
-					{
-						result +=fragmentSeperator;
-					}
-					else
-					{
-						first=false;
-					}
-					result +=frag.toString();
-				}
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvalidTokenOffsetsException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return(result);
-	}
+  /**
+   * Highlights Terms by enclosing them with &lt;b&gt;term&lt;/b&gt;
+   * @param originalTermText 
+   * @param tokenGroup 
+   * @return 
+   */
+  public String highlightTerm(String originalTermText, TokenGroup tokenGroup) {
+    UseCase uc = MonitorFactory.startUseCase(
+    "Highlight.PhraseBolder.highlightTerm()");
+    if (tokenGroup.getTotalScore() <= 0) {
+      uc.stop();
+      return originalTermText;
+    }
+    uc.stop();
+    return highlightPrefix + originalTermText + highlightPostfix;
+
+  }
+
+
+  /**
+   * @param attribute 
+   * @param parsedQuery 
+   * @return 
+   * 
+   */
+  public String highlight(String attribute, Query parsedQuery) {
+    UseCase uc = MonitorFactory.startUseCase(
+        "Highlight.PhraseBolder.highlight()");
+    String result = "";
+    if (attribute != null && parsedQuery != null) {
+      Highlighter highlighter =
+        new Highlighter(this, new QueryScorer(parsedQuery));
+      highlighter.setTextFragmenter(new WordCountFragmenter(fragmentSize));
+
+      TokenStream tokenStream = analyzer.tokenStream(
+          this.getHighlightAttribute(), new StringReader(attribute));
+      try {
+        UseCase ucFragments = MonitorFactory.startUseCase(
+        "Highlight.PhraseBolder.highlight()#getFragments");
+        TextFragment[] frags = highlighter.getBestTextFragments(tokenStream,
+            attribute, true, numMaxFragments);
+        ucFragments.stop();
+        boolean first = true;
+        for (TextFragment frag : frags) {
+          if (!first) {
+            result += fragmentSeperator;
+          } else {
+            first = false;
+          }
+          result += frag.toString();
+        }
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (InvalidTokenOffsetsException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+    uc.stop();
+    return result;
+  }
 
 }
