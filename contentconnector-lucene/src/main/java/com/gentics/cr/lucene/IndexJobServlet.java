d737b5fda09f3ba25fa12b4d2cab0ed972e410ef
paul.cervenka@gmail.com <paul.cervenka@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-07-23 11:23:04 +0000

    using velocity template for rendering indexing information

--- contentconnector-lucene/src/main/java/com/gentics/cr/lucene/IndexJobServlet.java
+++ contentconnector-lucene/src/main/java/com/gentics/cr/lucene/IndexJobServlet.java
@@ -1,7 +1,6 @@
 package com.gentics.cr.lucene;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Hashtable;
 import java.util.Map.Entry;
@@ -15,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import com.gentics.cr.CRConfigUtil;
+import com.gentics.cr.template.FileTemplate;
+import com.gentics.cr.template.ITemplate;
+import com.gentics.cr.template.ITemplateManager;
 import com.gentics.cr.util.indexing.AbstractUpdateCheckerJob;
 import com.gentics.cr.util.indexing.IndexController;
 import com.gentics.cr.util.indexing.IndexJobQueue;
@@ -30,9 +32,11 @@ public class IndexJobServlet extends HttpServlet {
   private static final String NAGIOS_PARAM = "nagios";
   private static final long serialVersionUID = 0002L;
   private Logger log;
-
   private IndexController indexer;
 
+  private CRConfigUtil crConf;
+  private ITemplateManager vtl;
+  private ITemplate tpl;
 
 
   public final void init(final ServletConfig config) throws ServletException {
@@ -41,6 +45,18 @@ public class IndexJobServlet extends HttpServlet {
     this.log = Logger.getLogger("com.gentics.cr.lucene");
     this.indexer = new IndexController(config.getServletName());
 
+    
+    
+    this.crConf = new CRConfigUtil();
+    this.vtl = crConf.getTemplateManager();
+    
+	try{
+		this.tpl = new FileTemplate(IndexJobServlet.class.getResourceAsStream("indexjobtemplate.vm"));
+	}
+	catch(Exception ex)
+	{
+		log.error("FAILED TO LOAD VELOCITY TEMPLATE FROM indexjobtemplate.vm");
+	}
   }
 
   @Override
@@ -75,7 +91,6 @@ public class IndexJobServlet extends HttpServlet {
       response.setContentType("text/plain");
       Hashtable<String, IndexLocation> indexTable = indexer.getIndexes();
       for (Entry<String, IndexLocation> e : indexTable.entrySet()) {
-
         if(e.getKey().equalsIgnoreCase(index))
         {
           IndexLocation loc = e.getValue();
@@ -94,26 +109,21 @@ public class IndexJobServlet extends HttpServlet {
           {
             response.getWriter().write("CurrentJobObjectsToIndex:"+j.getObjectsToIndex()+"\n");
           }
-
-
-
         }
       }
     } else {
       String nc = "&t="+System.currentTimeMillis();
 
       response.setContentType("text/html");
-      response.getWriter().write("<html>\r\n"+
-          "<head>\r\n" +
-          "<title>IndexerServlet for Lucene by Gentics</title>\r\n" +
-          "<meta http-equiv=\"refresh\" content=\"5; URL=\"?ts="+(new Date().getTime())+"\" />" +
-          "</head>\r\n" +
-          "<body>\r\n");
+      
       Hashtable<String,IndexLocation> indexTable = indexer.getIndexes();
+      
+      this.vtl.put("indexes", indexTable.entrySet());
+      this.vtl.put("nc", nc);
+      
       for (Entry<String,IndexLocation> e:indexTable.entrySet()) {
         
-        response.getWriter().write("<h1>Index "+e.getKey()+"</h1><br/>\n");
-        IndexLocation loc = e.getValue();
+    	IndexLocation loc = e.getValue();
         IndexJobQueue queue = loc.getQueue();
         
         Hashtable<String,CRConfigUtil> map = loc.getCRMap();
@@ -139,47 +149,19 @@ public class IndexJobServlet extends HttpServlet {
             }
           }
         }
-        response.getWriter().write("ObjectsInIndex: "+loc.getDocCount()+"<br/>\n");
-        ArrayList<AbstractUpdateCheckerJob> lastJobs = queue.getLastJobs();
-        for(AbstractUpdateCheckerJob lj:lastJobs)
-        {
-          response.getWriter().write("Job "+lj.getIdentifyer()+" took "+lj.getDuration()+"ms for "+lj.getObjectsDone()+" objects<br/>\n");
-        }
-        
-        response.getWriter().write(queue.getSize()+" Elements in queue<br/>\n");
-        response.getWriter().write("Current interval: "+queue.getInterval()+"s<br/>\n");
-        if(loc.isPeriodical())
-        {
-          response.getWriter().write("All CRs will be indexed periodical<br/>\n");
-        }
-        if(queue.isRunning())
-        {
-          response.getWriter().write("Worker is processing queue (<a href=\"?action=stopWorker&idx="+e.getKey()+nc+"\">stop</a>)<br/>\n");
-        }
-        else
-        {
-          response.getWriter().write("Worker not running (<a href=\"?action=startWorker&idx="+e.getKey()+nc+"\">start</a>)<br/>\n");
-        }
-        AbstractUpdateCheckerJob j = queue.getCurrentJob();
-
-        if (j != null) {
-          response.getWriter().write("<hr><br/>\n");
-          response.getWriter().write("Current Job "+j.getIdentifyer()+"<br/>\n");
-          response.getWriter().write("Current Status: "+j.getStatusString()+"<br/>\n");
-          response.getWriter().write("Objects to index "+j.getObjectsToIndex()+"<br/>\n");
-          response.getWriter().write("Objects done "+j.getObjectsDone()+"<br/>\n");
-          response.getWriter().write("<hr><br/>\n");
-        }
-        for (Entry<String,CRConfigUtil> crc:map.entrySet()) {
-          response.getWriter().write("Add CR "+crc.getKey()+" for indexing (<a href=\"?action=addJob&idx="+e.getKey()+"&cr="+crc.getKey()+nc+"\">add</a>)<br/>\n");
-        }
-        response.getWriter().write("Add all CRs for indexing (<a href=\"?action=addJob&idx="+e.getKey()+"&cr=all"+nc+"\">add</a>)<br/>\n");
-        
       }
-      
-      response.getWriter().write("<a href=\"?action=show"+nc+"\">Refresh status</a><br/>\n");
-      response.getWriter().write("</body></html>");
     }
+    
+	try
+	{
+		String output = this.vtl.render(this.tpl.getKey(), this.tpl.getSource());
+		response.getWriter().write(output);
+	}
+	catch(Exception ex)
+	{
+		ex.printStackTrace();
+	}
+    
     response.getWriter().flush();
     response.getWriter().close();
     // endtime
