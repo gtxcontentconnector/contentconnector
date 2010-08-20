254a57fa9d7bdec66091127ac8403127dfe3452b
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-08-20 09:44:14 +0000

    added simple report

--- contentconnector-core/src/main/java/com/gentics/cr/monitoring/MonitorFactory.java
+++ contentconnector-core/src/main/java/com/gentics/cr/monitoring/MonitorFactory.java
@@ -1,5 +1,7 @@
 package com.gentics.cr.monitoring;
 
+import java.text.DecimalFormat;
+
 import com.gentics.cr.configuration.GenericConfiguration;
 
 public class MonitorFactory {
@@ -24,5 +26,51 @@ public class MonitorFactory {
 		}
 		return new UseCase(null,monitorenabled);
 		
+	}
+	
+	public static String getSimpleReport()
+	{
+		String ret = "<table class=\"report_table\">";
+		int max_field = 7;
+		
+		String[] header = com.jamonapi.MonitorFactory.getHeader();
+		ret+="<tr>";
+		for(int i=0;i<=max_field;i++)
+		{
+			ret+="<th>"+header[i]+"</th>";
+		}
+		ret+="</tr>";
+		
+		Object[][] data = com.jamonapi.MonitorFactory.getData();
+		
+		DecimalFormat output = new DecimalFormat("#0.00");
+		
+		for(int j=0;j<data.length;j++)
+		{
+			if((j%2) == 0)
+				ret+="<tr class=\"even\">";
+			else
+				ret+="<tr class=\"odd\">";
+			for(int i=0;i<=max_field;i++)
+			{
+				Object obj = data[j][i];
+				
+				if(obj instanceof Double)
+				{
+					obj = output.format(((Double)obj));
+				}
+				if(i>0)
+				{
+					ret+="<td class=\"value\">"+obj.toString()+"</td>";
+				}
+				else
+				{
+					ret+="<td>"+obj.toString()+"</td>";
+				}
+			}
+			ret+="</tr>";
+		}
+		ret+="</table>";
+		return ret;
 	}
 }
