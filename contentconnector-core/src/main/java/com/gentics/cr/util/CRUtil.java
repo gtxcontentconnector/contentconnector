ffef45ca263d3990847b163bef45f5779f6b220c
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-09-07 15:38:17 +0000

    ouput error messages for missing nodelog.properties and cache.ccf only once error messages for missing nodelog.properties are now put to System.out (since we cannot init log4j) error messages for missing cache.ccf are put on System.out when no nodelog.properties was found so the user has an additional hint.

--- contentconnector-core/src/main/java/com/gentics/cr/util/CRUtil.java
+++ contentconnector-core/src/main/java/com/gentics/cr/util/CRUtil.java
@@ -19,166 +19,171 @@ import com.gentics.api.lib.datasource.Datasource.Sorting;
  *
  */
 public class CRUtil {
-	
-	/**
-	 * Convert a String like "contentid:asc,name:desc" into an Sorting Array.
-	 * Errorhandling or errorrcognition is poor at that time.
-	 * 
-	 * @param sortingString like "contentid:asc,name:desc"
-	 * @return a Sorting[] Array suitable for contentrepository queries
-	 */
-	public final static Sorting[] convertSorting(String sortingString) {
 
-		String[] sortingArray;
-		HashSet<Sorting> sortingColl = new HashSet<Sorting>();
+  /**
+   * system property holding the Gentics Portal.Node confpath.
+   */
+  public static final String PORTALNODE_CONFPATH =
+    "com.gentics.portalnode.confpath";
 
-		if (sortingString != null) {
+  /**
+   * Convert a String like "contentid:asc,name:desc" into an Sorting Array.
+   * Errorhandling or errorrcognition is poor at that time.
+   * 
+   * @param sortingString like "contentid:asc,name:desc"
+   * @return a Sorting[] Array suitable for contentrepository queries
+   */
+  public final static Sorting[] convertSorting(String sortingString) {
 
-			// split sorting attributes on ,.
-			sortingArray = sortingString.split(",");
+    String[] sortingArray;
+    HashSet<Sorting> sortingColl = new HashSet<Sorting>();
 
-			// look at each attribute for sort direction
-			for (int i = 0; i < sortingArray.length; i++) {
+    if (sortingString != null) {
 
-				// split attribute on :. First element is attribute name the
-				// second is the direction
-				String[] sort = sortingArray[i].split(":");
+      // split sorting attributes on ,.
+      sortingArray = sortingString.split(",");
 
-				if (sort[0] != null) {
-					if ("asc".equals(sort[1].toLowerCase())) {
-						sortingColl.add(new Sorting(sort[0],
-								Datasource.SORTORDER_ASC));
-					} else if ("desc".equals(sort[1].toLowerCase())) {
-						sortingColl.add(new Sorting(sort[0],
-								Datasource.SORTORDER_DESC));
-					} else {
-						sortingColl.add(new Sorting(sort[0],
-								Datasource.SORTORDER_NONE));
-					}
-				}
-			}
-		}
+      // look at each attribute for sort direction
+      for (int i = 0; i < sortingArray.length; i++) {
 
-		// convert hashmap to uncomfortable Array :-/ and return that or return
-		// null if converting need not succed for any reason.
-		if (sortingColl != null) {
-			return (Sorting[]) sortingColl.toArray(new Sorting[sortingColl
-					.size()]);
-		} else {
-			return null;
-		}
-	}
-	
-	/**
-	 * Convert a HTML-conform sorting request multivalue parameter array
-	 * to a Sorting[], suitable for the contentrepository queries
-	 * 
-	 * @param sortingArray like ["contentid:asc","name:desc"]
-	 * @return a Sorting[] Array suitable for contentrepository queries
-	 */
-	public final static Sorting[] convertSorting(String[] sortingArray) {
-		Sorting[] ret = new Sorting[0];
-		ArrayList<Sorting> sortingColl = new ArrayList<Sorting>();
-		if(sortingArray!=null)
-		{
-			int arrlen = sortingArray.length;
-			
-			
-			for (int i = 0; i < arrlen; i++) {
-	
-				// split attribute on :. First element is attribute name the
-				// second is the direction
-				String[] sort = sortingArray[i].split(":");
-	
-				if (sort[0] != null) {
-					if ("asc".equals(sort[1].toLowerCase())) {
-						sortingColl.add(new Sorting(sort[0],
-								Datasource.SORTORDER_ASC));
-					} else if ("desc".equals(sort[1].toLowerCase())) {
-						sortingColl.add(new Sorting(sort[0],
-								Datasource.SORTORDER_DESC));
-					} else {
-						sortingColl.add(new Sorting(sort[0],
-								Datasource.SORTORDER_NONE));
-					}
-				}
-			}
-		}
-		return(sortingColl.toArray(ret));
-	}
-	
-	
-	/**
-	 * Convert an array of strings to an array that can be usen in a filter rule
-	 * @param arr
-	 * @return rule ready string
-	 */
-	public static String prepareParameterArrayForRule(String[] arr)
-	{
-		String ret = "[";
-		int arrlen = arr.length;
-		if(arrlen>0)
-		{
-			ret+="'"+arr[0]+"'";
-			if(arrlen>1)
-			{
-				for(int i=1; i<arrlen;i++)
-				{
-					ret+=",'"+arr[i]+"'";
-				}
-			}
-		}
-		ret+="]";
-		return(ret);
-	}
-	
-	
-	
-	
-	
+        // split attribute on :. First element is attribute name the
+        // second is the direction
+        String[] sort = sortingArray[i].split(":");
+
+        if (sort[0] != null) {
+          if ("asc".equals(sort[1].toLowerCase())) {
+            sortingColl.add(new Sorting(sort[0],
+                Datasource.SORTORDER_ASC));
+          } else if ("desc".equals(sort[1].toLowerCase())) {
+            sortingColl.add(new Sorting(sort[0],
+                Datasource.SORTORDER_DESC));
+          } else {
+            sortingColl.add(new Sorting(sort[0],
+                Datasource.SORTORDER_NONE));
+          }
+        }
+      }
+    }
+
+    // convert hashmap to uncomfortable Array :-/ and return that or return
+    // null if converting need not succed for any reason.
+    if (sortingColl != null) {
+      return (Sorting[]) sortingColl.toArray(new Sorting[sortingColl
+          .size()]);
+    } else {
+      return null;
+    }
+  }
+  
+  /**
+   * Convert a HTML-conform sorting request multivalue parameter array
+   * to a Sorting[], suitable for the contentrepository queries
+   * 
+   * @param sortingArray like ["contentid:asc","name:desc"]
+   * @return a Sorting[] Array suitable for contentrepository queries
+   */
+  public final static Sorting[] convertSorting(String[] sortingArray) {
+    Sorting[] ret = new Sorting[0];
+    ArrayList<Sorting> sortingColl = new ArrayList<Sorting>();
+    if(sortingArray!=null)
+    {
+      int arrlen = sortingArray.length;
+      
+      
+      for (int i = 0; i < arrlen; i++) {
+  
+        // split attribute on :. First element is attribute name the
+        // second is the direction
+        String[] sort = sortingArray[i].split(":");
+  
+        if (sort[0] != null) {
+          if ("asc".equals(sort[1].toLowerCase())) {
+            sortingColl.add(new Sorting(sort[0],
+                Datasource.SORTORDER_ASC));
+          } else if ("desc".equals(sort[1].toLowerCase())) {
+            sortingColl.add(new Sorting(sort[0],
+                Datasource.SORTORDER_DESC));
+          } else {
+            sortingColl.add(new Sorting(sort[0],
+                Datasource.SORTORDER_NONE));
+          }
+        }
+      }
+    }
+    return(sortingColl.toArray(ret));
+  }
+  
+  
+  /**
+   * Convert an array of strings to an array that can be usen in a filter rule
+   * @param arr
+   * @return rule ready string
+   */
+  public static String prepareParameterArrayForRule(String[] arr)
+  {
+    String ret = "[";
+    int arrlen = arr.length;
+    if(arrlen>0)
+    {
+      ret+="'"+arr[0]+"'";
+      if(arrlen>1)
+      {
+        for(int i=1; i<arrlen;i++)
+        {
+          ret+=",'"+arr[i]+"'";
+        }
+      }
+    }
+    ret+="]";
+    return(ret);
+  }
+
+  /**
+   * pattern to find system properties.
+   */
+  private static Pattern findSystemProperties =
+    Pattern.compile("\\$\\{([a-zA-Z0-9\\._]+)\\}");
+
     /**
-     * pattern to find system properties
-     */
-    protected static Pattern findSystemProperties = Pattern
-            .compile("\\$\\{([a-zA-Z0-9\\._]+)\\}");
-
-    /**
-     * FIXME copied from {@link com.gentics.lib.etc.StringUtils} - move it to API ?
-     * 
-     * 
+     * FIXME copied from {@link com.gentics.lib.etc.StringUtils}
+     * - move it to API ?
      * Resolve system properties encoded in the string as ${property.name}
      * @param string string holding encoded system properties
      * @return string with the system properties resolved
      */
-    public static String resolveSystemProperties(String string) {
+    public static String resolveSystemProperties(final String string) {
         // avoid NPE here
         if (string == null) {
             return null;
         }
         //init com.gentics.portalnode.confpath if it isn't set
-        if(System.getProperty("com.gentics.portalnode.confpath") == null || System.getProperty("com.gentics.portalnode.confpath").equals("")){
-          System.setProperty("com.gentics.portalnode.confpath", System.getProperty("catalina.base")+File.separator+"conf"+File.separator+"gentics"+File.separator);
+        if (System.getProperty(PORTALNODE_CONFPATH) == null
+            || System.getProperty(PORTALNODE_CONFPATH).equals("")) {
+          String defaultConfPath = System.getProperty("catalina.base")
+          + File.separator + "conf" + File.separator + "gentics"
+          + File.separator;
+          System.setProperty(PORTALNODE_CONFPATH, defaultConfPath);
         }
         // create a matcher
         Matcher m = findSystemProperties.matcher(string);
         StringBuffer output = new StringBuffer();
         int startIndex = 0;
-        while(m.find()) {
-            // copy static string between the last found system property and this one
-            if(m.start() > startIndex) {
+        while (m.find()) {
+            // copy static string between the last found system property and
+            // this one
+            if (m.start() > startIndex) {
                 output.append(string.substring(startIndex, m.start()));
             }
             output.append(System.getProperty(m.group(1), ""));
             startIndex = m.end();
         }
         // if some trailing static string exists, copy it
-        if(startIndex < string.length()) {
+        if (startIndex < string.length()) {
             output.append(string.substring(startIndex));
         }
-
         return output.toString();
     }
-    
+
     /**
      * Slurps a InputStream into a String
      * @param in
@@ -186,13 +191,13 @@ public class CRUtil {
      * @throws IOException
      */
     public static String inputStreamToString (InputStream in) throws IOException {
-		StringBuffer out = new StringBuffer();
-	    byte[] b = new byte[4096];
-	    for (int n; (n = in.read(b)) != -1;) {
-	        out.append(new String(b, 0, n));
-	    }
-	    return out.toString();
-	}
+    StringBuffer out = new StringBuffer();
+      byte[] b = new byte[4096];
+      for (int n; (n = in.read(b)) != -1;) {
+          out.append(new String(b, 0, n));
+      }
+      return out.toString();
+  }
     
     /**
      * Slurps a Reader into a String
@@ -201,33 +206,31 @@ public class CRUtil {
      * @throws IOException
      */
     public static String readerToString (Reader in) throws IOException {
-		StringBuffer out = new StringBuffer();
-	    char[] b = new char[4096];
-	    for (int n; (n = in.read(b)) != -1;) {
-	        out.append(new String(b, 0, n));
-	    }
-	    return out.toString();
-	}
-    
-    /**
-     * Slurps a Reader into a String and strips the character 0
-     * @param in
-     * @return
-     * @throws IOException
-     */
-    public static String readerToPrintableCharString(Reader in)throws IOException
+    StringBuffer out = new StringBuffer();
+      char[] b = new char[4096];
+      for (int n; (n = in.read(b)) != -1;) {
+          out.append(new String(b, 0, n));
+      }
+      return out.toString();
+  }
+  /**
+   * Slurps a Reader into a String and strips the character 0
+   * @param in
+   * @return
+   * @throws IOException
+   */
+  public static String readerToPrintableCharString(Reader in)throws IOException
+  {
+    StringBuffer out = new StringBuffer();
+    int n;
+    while((n = in.read())!=-1)
     {
-    	StringBuffer out = new StringBuffer();
-	    int n;
-	    while((n = in.read())!=-1)
-	    {
-	    	if(n!=0)
-	    	{
-	    		char c = (char)n;
-	    		out.append(c);
-	    	}
-	    }
-	    return out.toString();
+      if(n!=0)
+      {
+        char c = (char)n;
+        out.append(c);
+      }
     }
-
+    return out.toString();
+  }
 }
