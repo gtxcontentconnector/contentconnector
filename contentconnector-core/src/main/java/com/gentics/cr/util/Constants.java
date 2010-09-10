3256a57053c0007efa50e8da119758418a831edb
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-09-10 08:21:20 +0000

    added constants and package information

---
+++ contentconnector-core/src/main/java/com/gentics/cr/util/Constants.java
@@ -0,0 +1,20 @@
+package com.gentics.cr.util;
+
+/**
+ * Class containing default constants that are often used througout the code.
+ * @author Christopher
+ *
+ */
+public final class Constants {
+	
+	/**
+	 * Milliseconds in a second.
+	 */
+	public static final int MILLISECONDS_IN_A_SECOND = 1000;
+	
+	/**
+	 * Prevents from instantiation.
+	 */
+	private Constants() { }
+
+}
