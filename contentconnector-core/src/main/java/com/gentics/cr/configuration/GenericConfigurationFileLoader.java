03b66d80d299de3c9d39c5643aaa6909f5a95d97
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-09-10 08:18:09 +0000

    Added javadoc/checkstyle and extracted convinience methods into AccessibleConfiguration

--- contentconnector-core/src/main/java/com/gentics/cr/configuration/GenericConfigurationFileLoader.java
+++ contentconnector-core/src/main/java/com/gentics/cr/configuration/GenericConfigurationFileLoader.java
@@ -11,7 +11,7 @@ import org.apache.log4j.Logger;
 import com.gentics.cr.util.CRUtil;
 
 /**
- * Loads the contents of a file to a GenericConfiguration
+ * Loads the contents of a file to a GenericConfiguration.
  * 
  * 
  * Last changed: $Date: 2010-04-01 15:25:54 +0200 (Do, 01 Apr 2010) $
@@ -20,50 +20,70 @@ import com.gentics.cr.util.CRUtil;
  *
  */
 public class GenericConfigurationFileLoader {
-	private static Logger log = Logger.getLogger(GenericConfigurationFileLoader.class);
+	/**
+	 * Logger.
+	 */
+	private static Logger log = Logger.getLogger(
+			GenericConfigurationFileLoader.class);
 	
 	/**
-	 * Loads the contents of a Properties file that is located in the rest directory to the passed GenericConfiguration
+	 * Prevent instantiation.
+	 */
+	private GenericConfigurationFileLoader() { }
+	
+	/**
+	 * Loads the contents of a Properties file that is located in the rest 
+	 * directory to the passed GenericConfiguration.
 	 * @param config - the configuration to load the properties to
 	 * @param servletname - will be used to identify the file
-	 * 						e.g.: ${com.gentics.portalnode.confpath}/rest/<servletname>.properties
+	 * 						e.g.: ${com.gentics.portalnode.confpath}
+	 * /rest/<servletname>.properties
 	 */
-	public static void loadPerServletname(GenericConfiguration config, String servletname)
-	{
-		String path = CRUtil.resolveSystemProperties("${com.gentics.portalnode.confpath}/rest/"+servletname+".properties");
+	public static void loadPerServletname(GenericConfiguration config, 
+			final String servletname) {
+		String path = CRUtil.resolveSystemProperties(
+				"${com.gentics.portalnode.confpath}/rest/"
+				+ servletname + ".properties");
 		try {
-			load(config,path);
+			load(config, path);
 		} catch (FileNotFoundException e) {
-			log.error("Could not load configuration from "+path, e);
+			log.error("Could not load configuration from " + path, e);
 		} catch (IOException e) {
-			log.error("Could not load configuration from "+path, e);
+			log.error("Could not load configuration from " + path, e);
 		}
 	}
 	
 	/**
-	 * Loads the contents of a Properties file to the passed GenericConfiguration
+	 * Loads the contents of a Properties file 
+	 * to the passed GenericConfiguration.
 	 * @param config - configuration to load Files to
-	 * @param path - Path to the configuration File. Can contain environment variables.
+	 * @param path - Path to the configuration File. 
+	 * Can contain environment variables.
 	 * @throws IOException 
-	 * @throws FileNotFoundException 
 	 */
-	public static void load(GenericConfiguration config, String path) throws FileNotFoundException, IOException
-	{
+	public static void load(GenericConfiguration config, 
+			final String path) throws IOException {
 		Properties props = new Properties();
 		props.load(new FileInputStream(CRUtil.resolveSystemProperties(path)));
-		for (Entry<Object,Object> entry:props.entrySet()) {
+		for (Entry<Object, Object> entry : props.entrySet()) {
 			Object value = entry.getValue();
 			Object key = entry.getKey();
-			setProperty(config,(String)key, (String)value);
+			setProperty(config, (String) key, (String) value);
 		}
 	}
 	
-	private static void setProperty(GenericConfiguration config,String key, String value)
-	{
+	/**
+	 * Set a property to a generic configuration.
+	 * @param config configuration where the property should be set
+	 * @param key resolving key
+	 * @param value value
+	 */
+	private static void setProperty(GenericConfiguration config,
+			final String key, final String value) {
 		//Resolve system properties, so that they can be used in config values
-		value = CRUtil.resolveSystemProperties((String)value);
+		String val = CRUtil.resolveSystemProperties((String) value);
 				
 		//Set the property
-		config.set(key,value);
+		config.set(key, val);
 	}
 }
--- contentconnector-core/src/main/java/com/gentics/cr/configuration/StaticConfigurationContainer.java
+++ contentconnector-core/src/main/java/com/gentics/cr/configuration/StaticConfigurationContainer.java
@@ -6,48 +6,60 @@ import org.apache.log4j.Logger;
 
 import com.gentics.cr.CRConfigFileLoader;
 /**
- * 
+ * Class that acts as configuration container.
  * Last changed: $Date: 2009-09-02 17:57:48 +0200 (Mi, 02 Sep 2009) $
  * @version $Revision: 180 $
  * @author $Author: supnig@constantinopel.at $
  *
  */
-public class StaticConfigurationContainer {
-	
-	private static Logger log = Logger.getLogger(StaticConfigurationContainer.class);
-	private static Hashtable<String,CRConfigFileLoader> configmap = new Hashtable<String,CRConfigFileLoader>(2);
+public final class StaticConfigurationContainer {
 	
 	/**
-	 * Fetches a already created instance of the requested config. If config was not created yet, one will be instantiated.
-	 * @param key
-	 * @param webapproot
-	 * @return
+	 * Logger.
 	 */
-	public static CRConfigFileLoader getConfig(String key, String webapproot){
+	private static Logger log = Logger
+		.getLogger(StaticConfigurationContainer.class);
+	/**
+	 * Cache for parsed configs.
+	 */
+	private static Hashtable<String, CRConfigFileLoader> configmap 
+		= new Hashtable<String, CRConfigFileLoader>(2);
+	
+	/**
+	 * Prevent instantiation.
+	 */
+	private StaticConfigurationContainer() { }
+	
+	/**
+	 * Fetches a already created instance of the requested config. 
+	 * If config was not created yet, one will be instantiated.
+	 * @param key konfiguration key. e.g. servlet name
+	 * @param webapproot root of the file location
+	 * @return Config as <code>CRConfigFileLoader</code>
+	 */
+	public static CRConfigFileLoader getConfig(final String key, 
+				final String webapproot) {
 		return StaticConfigurationContainer.getConfig(key, webapproot, "");
 	}
 	
 	/**
-	 * Gets configuration
-	 * @param key
-	 * @param webapproot
-	 * @param subdir
-	 * @return
+	 * Gets configuration.
+	 * @param key configuration key.
+	 * @param webapproot root of file location.
+	 * @param subdir sub directory.
+	 * @return Configuration as <code>CRConfigFileLoader</code>
 	 */
-	public static CRConfigFileLoader getConfig(String key, String webapproot, String subdir)
-	{
+	public static CRConfigFileLoader getConfig(final String key,
+			final String webapproot, final String subdir) {
 		CRConfigFileLoader config = configmap.get(key);
-		if(config==null)
-		{
+		if (config == null) {
 			log.debug("Config not found, will create new config instance.");
-			config = new CRConfigFileLoader(key,webapproot,subdir);
-			if(config!=null)
-			{
+			config = new CRConfigFileLoader(key, webapproot, subdir);
+			if (config != null) {
 				configmap.put(key, config);
 			}
 		}
-		
-		
+
 		return config;
 	}
 }
