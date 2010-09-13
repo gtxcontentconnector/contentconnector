35b0da546f4fdd3e90280a6dafdaf6a471c63472
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-09-13 08:55:05 +0000

    handle exceptions with logger not with printStackTrace

--- contentconnector-core/src/main/java/com/gentics/cr/CRRequestProcessor.java
+++ contentconnector-core/src/main/java/com/gentics/cr/CRRequestProcessor.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
+
 import com.gentics.api.lib.datasource.Datasource;
 import com.gentics.api.lib.datasource.DatasourceException;
 import com.gentics.api.lib.exception.ParserException;
@@ -22,104 +24,114 @@ import com.gentics.cr.util.ArrayHelper;
  *
  */
 public class CRRequestProcessor extends RequestProcessor {
+	
+	private static Logger logger = Logger.getLogger(CRRequestProcessor.class);
 
-  private HashMap<String, Resolvable> resolvables = null;
-  
-  /**
-   * Create a new instance of CRRequestProcessor
-   * @param config
-   * @throws CRException
-   */
-  public CRRequestProcessor(CRConfig config) throws CRException {
-    super(config);
-  }
-  
-  
-  
-  /**
-   * 
-   * Fetch the matching objects using the given CRRequest. 
-   * @param request CRRequest
-   * @param doNavigation defines if to fetch child elements
-   * @return resulting objects
-   * @throws CRException 
-   */
-  public Collection<CRResolvableBean> getObjects(CRRequest request, boolean doNavigation) throws CRException
-  {
-    Datasource ds = null;
-    DatasourceFilter dsFilter;
-    Vector<CRResolvableBean> collection = new Vector<CRResolvableBean>();
-    if (request != null) {
+	private HashMap<String, Resolvable> resolvables = null;
+	
+	/**
+	 * Create a new instance of CRRequestProcessor
+	 * @param config
+	 * @throws CRException
+	 */
+	public CRRequestProcessor(CRConfig config) throws CRException {
+		super(config);
+	}
+	
+	
+	
+	/**
+	 * 
+	 * Fetch the matching objects using the given CRRequest. 
+	 * @param request CRRequest
+	 * @param doNavigation defines if to fetch child elements
+	 * @return resulting objects
+	 * @throws CRException 
+	 */
+	public Collection<CRResolvableBean> getObjects(CRRequest request, boolean doNavigation) throws CRException
+	{
+		Datasource ds = null;
+		DatasourceFilter dsFilter;
+		Vector<CRResolvableBean> collection = new Vector<CRResolvableBean>();
+		if (request != null) {
 
-      // Parse the given expression and create a datasource filter
-      try {
-        ds = this.config.getDatasource();
-        if (ds == null) {
-          throw (new DatasourceException("No Datasource available."));
-        }
+			// Parse the given expression and create a datasource filter
+			try {
+				ds = this.config.getDatasource();
+				if (ds == null) {
+					throw (new DatasourceException("No Datasource available."));
+				}
 
-        dsFilter = request.getPreparedFilter(config, ds);
+				dsFilter = request.getPreparedFilter(config, ds);
 
-        // add base resolvables
-        if (this.resolvables != null) {
-          for (Iterator<String> it = this.resolvables.keySet().iterator(); it
-              .hasNext();) {
-            String name = it.next();
-            dsFilter.addBaseResolvable(name,
-                this.resolvables.get(name));
-          }
-        }
+				// add base resolvables
+				if (this.resolvables != null) {
+					for (Iterator<String> it = this.resolvables.keySet()
+							.iterator(); it.hasNext();) {
+						String name = it.next();
+						dsFilter.addBaseResolvable(name,
+								this.resolvables.get(name));
+					}
+				}
 
-        String[] prefillAttributes =  request.getAttributeArray();
-        prefillAttributes = ArrayHelper.removeElements(prefillAttributes,
-            "contentid", "updatetimestamp");
-        // do the query
-        Collection<Resolvable> col = this.toResolvableCollection(ds.getResult(
-            dsFilter, prefillAttributes, request.getStart().intValue(),
-            request.getCount().intValue(), request.getSorting()));
+				String[] prefillAttributes = request.getAttributeArray();
+				prefillAttributes = ArrayHelper
+						.removeElements(prefillAttributes, "contentid",
+								"updatetimestamp");
+				// do the query
+				Collection<Resolvable> col = this.toResolvableCollection(
+						ds.getResult(dsFilter, prefillAttributes,
+								request.getStart().intValue(),
+								request.getCount().intValue(),
+								request.getSorting()));
 
-        // convert all objects to serializeable beans
-        if (col != null) {
-          for (Iterator<Resolvable> it = col.iterator(); it.hasNext();) {
-            CRResolvableBean crBean = new CRResolvableBean(
-                it.next(), request.getAttributeArray());
-            if (this.config.getFolderType().equals(crBean.getObj_type())
-                && doNavigation) {
-              //Process child elements
-              String fltr="object.folder_id=='"+crBean.getContentid()+"'";
-              if(request.getChildFilter()!=null)
-                fltr+="AND ("+request.getChildFilter()+")";
-              //If object is a folder => retrieve the childs of the object
-              CRRequest childReq = request.Clone();
-              childReq.setRequestFilter(fltr);
-              crBean.fillChildRepository(this.getNavigation(childReq));
-            }
-            collection.add(this.replacePlinks(crBean, request));
-          }
-        }
+				// convert all objects to serializeable beans
+				if (col != null) {
+					for (Iterator<Resolvable> it = col.iterator(); it.hasNext();) {
+						CRResolvableBean crBean = new CRResolvableBean(
+								it.next(), request.getAttributeArray());
+						if (this.config.getFolderType().equals(crBean.getObj_type())
+								&& doNavigation) {
+							//Process child elements
+							String fltr = "object.folder_id=='"
+								+ crBean.getContentid() + "'";
+							if (request.getChildFilter() != null) {
+								fltr += "AND (" + request.getChildFilter()
+									+ ")";
+							}
+							//If object is a folder => retrieve the children of
+							//the object
+							CRRequest childReq = request.Clone();
+							childReq.setRequestFilter(fltr);
+							crBean.fillChildRepository(
+									this.getNavigation(childReq));
+						}
+						collection.add(this.replacePlinks(crBean, request));
+					}
+				}
 
-      } catch (ParserException e) {
-        e.printStackTrace();
-        throw new CRException("ParserException", e.getMessage());
-      } catch (ExpressionParserException e) {
-        e.printStackTrace();
-        throw new CRException("ExpressionParserException", e.getMessage());
-      } catch (DatasourceException e) {
-        e.printStackTrace();
-        throw new CRException("DatasourceException", e.getMessage());
-      } finally {
-        CRDatabaseFactory.releaseDatasource(ds);
-      }
-    }
-    return collection;
-  }
+			} catch (ParserException e) {
+				logger.error("Error getting filter for Datasource.", e);
+				throw new CRException(e);
+			} catch (ExpressionParserException e) {
+				logger.error("Error getting filter for Datasource.", e);
+				throw new CRException(e);
+			} catch (DatasourceException e) {
+				logger.error("Error getting result from Datasource.", e);
+				throw new CRException(e);
+			} finally {
+				CRDatabaseFactory.releaseDatasource(ds);
+			}
+		}
+		return collection;
+	}
 
 
 
-  @Override
-  public void finalize() {
-    // TODO Auto-generated method stub
-  }
+	@Override
+	public void finalize() {
+		// TODO Auto-generated method stub
+	}
 
 
 
