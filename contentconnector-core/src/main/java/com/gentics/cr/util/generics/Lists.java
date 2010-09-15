798928c5230f1edf4604ed6db3fe028a758fcbdb
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-09-15 13:00:46 +0000

    added method to convert raw objects into lists (if they are lists of course)

--- contentconnector-core/src/main/java/com/gentics/cr/util/generics/Lists.java
+++ contentconnector-core/src/main/java/com/gentics/cr/util/generics/Lists.java
@@ -12,6 +12,24 @@ public final class Lists {
 	 */
 	private Lists() { }
 	
+	
+	/**
+	 * Convert a list of objects into a specified list of objects.
+	 * @param <T> - Type of objects in target list
+	 * @param list - list to convert into list of T
+	 * @param clazz - Class to cast elements into
+	 * @return list of objects of type T, <code>null</code> in case list is not
+	 * a List
+	 */
+	public static <T> List<T> toSpecialList(final Object list,
+			final Class<T> clazz) {
+		if (list instanceof List) {
+			return toSpecialList((List<?>) list, clazz);
+		} else {
+			return null;
+		}
+	}
+	
 	/**
 	 * Convert a list of objects into a specified list of objects.
 	 * @param <T> - Type of objects in target list
@@ -20,7 +38,6 @@ public final class Lists {
 	 * @param clazz - Class to cast elements into
 	 * @return list of objects of type T
 	 */
-	//https://bugs.eclipse.org/bugs/show_bug.cgi?id=74420
 	public static <S, T extends S> List<T> toSpecialList(final List<S> list,
 			final Class<T> clazz) {
 		List<T> result = new ArrayList<T>(list.size());
