1b06c50f812f4f8503a83dd4f44e4668f57221b8
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-09-16 13:58:07 +0000

    reformated an javadoc

--- contentconnector-lucene/src/main/java/com/gentics/cr/lucene/indexer/transformer/MergeTransformer.java
+++ contentconnector-lucene/src/main/java/com/gentics/cr/lucene/indexer/transformer/MergeTransformer.java
@@ -4,47 +4,71 @@ import org.apache.log4j.Logger;
 
 import com.gentics.cr.CRResolvableBean;
 import com.gentics.cr.configuration.GenericConfiguration;
-import com.gentics.cr.lucene.indexer.transformer.ContentTransformer;
 /**
- * Sets a configured value
+ * Merge one or more attributes into another attribute.
  * @author Paul Cervenka
  *
  */
 public class MergeTransformer extends ContentTransformer {
 
-	private static final String TRANSFORMER_SOUCREATTRIBUTE_KEY="sourceattributes";
-	private static final String TRANSFORMER_TARGETATTRIBUTE_KEY="targetattribute";
-	private String[] source_attributes;
-	private String target_attribute;
+	/**
+	 * Configuration key for source attribute.
+	 */
+	private static final String TRANSFORMER_SOUCREATTRIBUTE_KEY =
+		"sourceattributes";
+	/**
+	 * Configuration key for target attribute.
+	 */
+	private static final String TRANSFORMER_TARGETATTRIBUTE_KEY =
+		"targetattribute";
 	
+	/**
+	 * Array of attributes to merge.
+	 */
+	private String[] sourceAttributes;
+	/**
+	 * attribute name to store the merged attributes in.
+	 */
+	private String targetAttribute;
+	
+	/**
+	 * Log4j logger for debug and error messages.
+	 */
 	private static Logger logger = Logger.getLogger(MergeTransformer.class);
 	
 	/**
-	 * Creates instance of SetValueTransformer
-	 * @param config
+	 * Creates instance of MergeTransformer.
+	 * @param config configuration for the MergeTransformer
 	 */
-	public MergeTransformer(GenericConfiguration config){
+	public MergeTransformer(final GenericConfiguration config) {
 		super(config);
 		
-		String source = (String)config.get(TRANSFORMER_SOUCREATTRIBUTE_KEY);
-		target_attribute = (String)config.get(TRANSFORMER_TARGETATTRIBUTE_KEY);
-		source_attributes = source.split(",");
+		String source = (String) config.get(TRANSFORMER_SOUCREATTRIBUTE_KEY);
+		targetAttribute = (String) config.get(TRANSFORMER_TARGETATTRIBUTE_KEY);
+		sourceAttributes = source.split(",");
 		
-		if (source_attributes == null) { logger.error("Please configure "+TRANSFORMER_SOUCREATTRIBUTE_KEY + " for my config."); }
-		if (target_attribute == null) { logger.error("Please configure "+TRANSFORMER_TARGETATTRIBUTE_KEY + " for my config."); }
+		if (sourceAttributes == null) {
+			logger.error("Please configure " + TRANSFORMER_SOUCREATTRIBUTE_KEY
+					+ " for my config.");
+		}
+		if (targetAttribute == null) {
+			logger.error("Please configure " + TRANSFORMER_TARGETATTRIBUTE_KEY
+					+ " for my config.");
+		}
 	}
 	
 	@Override
-	public void processBean(CRResolvableBean bean) {
+	public final void processBean(final CRResolvableBean bean) {
 		StringBuffer target;
 				
-		if (source_attributes != null && target_attribute != null) {
+		if (sourceAttributes != null && targetAttribute != null) {
 			target = new StringBuffer();
-			for(String attribute:source_attributes) {
-				if (bean.get(attribute) != null)
+			for (String attribute : sourceAttributes) {
+				if (bean.get(attribute) != null) {
 					target.append(bean.get(attribute)).append(" ");
+				}
 			}
-			bean.set(target_attribute, target.toString());
+			bean.set(targetAttribute, target.toString());
 		}
 	}
 
