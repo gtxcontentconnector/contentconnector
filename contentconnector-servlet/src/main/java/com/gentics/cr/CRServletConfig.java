43137cb1fbd1baf48a5b7cc4c97b8f28a9042220
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-10-05 09:01:20 +0000

    added some javadoc

--- contentconnector-servlet/src/main/java/com/gentics/cr/CRServletConfig.java
+++ contentconnector-servlet/src/main/java/com/gentics/cr/CRServletConfig.java
@@ -10,12 +10,16 @@ import javax.servlet.ServletConfig;
 public class CRServletConfig extends CRConfigFileLoader {
 
 	/**
-	 * 
+	 * generated serial version unique id.
 	 */
 	private static final long serialVersionUID = 5615582224307130579L;
 
+	/**
+	 * TODO javadoc
+	 * @param config TODO javadoc
+	 */
 	public CRServletConfig(ServletConfig config) {
-		super(config.getServletName(),config.getServletContext().getRealPath(""));		
+		super(config.getServletName(),config.getServletContext().getRealPath(""));
 	}
 	
 }
