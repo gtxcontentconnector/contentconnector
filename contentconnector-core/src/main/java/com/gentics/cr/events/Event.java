401401013cf6fabafb1d850a95d17f37f1cc13f7
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-12-16 16:16:28 +0000

    checkstyle

--- contentconnector-core/src/main/java/com/gentics/cr/events/Event.java
+++ contentconnector-core/src/main/java/com/gentics/cr/events/Event.java
@@ -1,14 +1,21 @@
 package com.gentics.cr.events;
 /**
- * 
+ * General Interface for Content Connector Events.
  * Last changed: $Date: 2009-07-10 10:49:03 +0200 (Fr, 10 Jul 2009) $
  * @version $Revision: 131 $
  * @author $Author: supnig@constantinopel.at $
  *
  */
 public abstract class Event {
-	
+	/**
+	 * Get the Type of the Event.
+	 * @return type as String
+	 */
 	public abstract String getType();
 	
+	/**
+	 * Get the Data of the Event.
+	 * @return data
+	 */
 	public abstract Object getData();
 }
