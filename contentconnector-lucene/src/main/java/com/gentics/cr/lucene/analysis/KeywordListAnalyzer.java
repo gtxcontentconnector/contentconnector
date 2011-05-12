73c59c7a830d9655c36239a645be3f0896919410
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-05-12 13:50:00 +0000

    added KeywordListAnalyzer

---
+++ contentconnector-lucene/src/main/java/com/gentics/cr/lucene/analysis/KeywordListAnalyzer.java
@@ -0,0 +1,34 @@
+package com.gentics.cr.lucene.analysis;
+
+import java.io.IOException;
+import java.io.Reader;
+ 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+ 
+/**
+ * "Tokenizes" the entire stream as a single token. This is useful
+ * for data like zip codes, ids, and some product names.
+ */
+public class KeywordListAnalyzer extends Analyzer {
+ 
+  @Override
+  public final TokenStream tokenStream(final String fieldName,
+		final Reader reader) {
+    return new KeywordListTokenizer(reader);
+  }
+  @Override
+  public final TokenStream reusableTokenStream(final String fieldName,
+		  final Reader reader) throws IOException {
+   
+    Tokenizer tokenizer = (Tokenizer) getPreviousTokenStream();
+    if (tokenizer == null) {
+      tokenizer = new KeywordListTokenizer(reader);
+      setPreviousTokenStream(tokenizer);
+    } else {
+      tokenizer.reset(reader);
+    }
+    return tokenizer;
+  }
+}
