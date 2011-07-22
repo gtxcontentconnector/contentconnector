e96dfd0cab0084b99b4452541e8b8e0b7286faaf
friesoft@gmail.com <friesoft@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-07-22 10:39:14 +0000

    fixed missing imports

--- contentconnector-core/src/main/java/com/gentics/cr/template/FileTemplate.java
+++ contentconnector-core/src/main/java/com/gentics/cr/template/FileTemplate.java
@@ -1,5 +1,7 @@
 package com.gentics.cr.template;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
