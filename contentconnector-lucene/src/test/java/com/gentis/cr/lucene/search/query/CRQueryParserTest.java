71bfb4b1438d52076fadad285ec021887962b81b
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-09-08 12:38:18 +0000

    use test1 and test2 in search because "a" is an english stopword

--- contentconnector-lucene/src/test/java/com/gentis/cr/lucene/search/query/CRQueryParserTest.java
+++ contentconnector-lucene/src/test/java/com/gentis/cr/lucene/search/query/CRQueryParserTest.java
@@ -1,13 +1,13 @@
 package com.gentis.cr.lucene.search.query;
 
+import junit.framework.TestCase;
+
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.queryParser.ParseException;
 
 import com.gentics.cr.CRRequest;
 import com.gentics.cr.lucene.LuceneVersion;
 import com.gentics.cr.lucene.search.query.CRQueryParser;
-
-import junit.framework.TestCase;
 
 public class CRQueryParserTest extends TestCase {
 	private static final StandardAnalyzer STANDARD_ANALYZER = new StandardAnalyzer(LuceneVersion.getVersion());
@@ -25,24 +25,26 @@ public class CRQueryParserTest extends TestCase {
 	}
 	
 	public void testReplaceBooleanMnoGoSearchQuery() throws ParseException {
-		assertEquals("(content:a OR binarycontent:a) OR (content:b OR binarycontent:b)", parser.parse("a | b"));
+		assertEquals("(content:test1 binarycontent:test1) (content:test2 binarycontent:test2)", parser.parse("test1 | test2").toString());
+		assertEquals("+(content:test1 binarycontent:test1) +(content:test2 binarycontent:test2)", parser.parse("test1 & test2").toString());
+		assertEquals("+(content:test1 binarycontent:test1) +(content:test2 binarycontent:test2)", parser.parse("test1&test2").toString());
 	}
 	public void testSearchAttributes() throws ParseException {
-		assertEquals("content:a binarycontent:a", parser.parse("a").toString());
+		assertEquals("content:test1 binarycontent:test1", parser.parse("test1").toString());
 	}
 	public void testWordMatch() throws ParseException {
 		crRequest.set(CRRequest.WORDMATCH_KEY, "sub");
 		parser = new CRQueryParser(LuceneVersion.getVersion(), SEARCHED_ATTRIBUTES, STANDARD_ANALYZER, crRequest);
-		assertEquals("content:*a* binarycontent:*a*", parser.parse("a").toString());
+		assertEquals("content:*test1* binarycontent:*test1*", parser.parse("test1").toString());
 		crRequest.set(CRRequest.WORDMATCH_KEY, "beg");
 		parser = new CRQueryParser(LuceneVersion.getVersion(), SEARCHED_ATTRIBUTES, STANDARD_ANALYZER, crRequest);
-		assertEquals("content:a* binarycontent:a*", parser.parse("a").toString());
+		assertEquals("content:test1* binarycontent:test1*", parser.parse("test1").toString());
 		crRequest.set(CRRequest.WORDMATCH_KEY, "end");
 		parser = new CRQueryParser(LuceneVersion.getVersion(), SEARCHED_ATTRIBUTES, STANDARD_ANALYZER, crRequest);
-		assertEquals("content:*a binarycontent:*a", parser.parse("a").toString());
+		assertEquals("content:*test1 binarycontent:*test1", parser.parse("test1").toString());
 		crRequest.set(CRRequest.WORDMATCH_KEY, "wrd");
 		parser = new CRQueryParser(LuceneVersion.getVersion(), SEARCHED_ATTRIBUTES, STANDARD_ANALYZER, crRequest);
-		assertEquals("content:a binarycontent:a", parser.parse("a").toString());
+		assertEquals("content:test1 binarycontent:test1", parser.parse("test1").toString());
 	}
 	
 }
