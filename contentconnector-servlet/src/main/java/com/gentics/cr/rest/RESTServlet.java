ddb9fa3ac2f39ce94802892c1fa42c1e04b6bd39
friesoft@gmail.com <friesoft@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-10-12 14:41:53 +0000

    coding style

--- contentconnector-servlet/src/main/java/com/gentics/cr/rest/RESTServlet.java
+++ contentconnector-servlet/src/main/java/com/gentics/cr/rest/RESTServlet.java
@@ -21,7 +21,6 @@ import com.gentics.cr.util.HttpSessionWrapper;
 import com.gentics.cr.util.RequestBeanWrapper;
 import com.gentics.cr.util.response.ServletResponseTypeSetter;
 
-
 /**
  * @author haymo
  * 
@@ -30,79 +29,81 @@ import com.gentics.cr.util.response.ServletResponseTypeSetter;
  */
 public class RESTServlet extends HttpServlet {
 
-  private static final long serialVersionUID = 0002L;
-  /**
-   * Log4j logger for debug and error messages.
-   */
-  private static Logger log = Logger.getLogger(RESTServlet.class);
-  /**
-   * Configuration for the Servlet.
-   */
-  private CRServletConfig crConf;
-  private RESTSimpleContainer container;
-  
-  
-  public void init(ServletConfig config) throws ServletException {
+	private static final long serialVersionUID = 0002L;
+	/**
+	 * Log4j logger for debug and error messages.
+	 */
+	private static Logger log = Logger.getLogger(RESTServlet.class);
+	/**
+	 * Configuration for the Servlet.
+	 */
+	private CRServletConfig crConf;
+	private RESTSimpleContainer container;
 
-    super.init(config);
-    crConf = new CRServletConfig(config);
-    container = new RESTSimpleContainer(crConf);
+	public void init(ServletConfig config) throws ServletException {
 
-  }
-  
-  @Override
-  public void destroy()
-  {
-    if(this.container!=null)this.container.finalize();
-  }
+		super.init(config);
+		crConf = new CRServletConfig(config);
+		container = new RESTSimpleContainer(crConf);
 
-  /**
-   * Wrapper Method for the doGet and doPost Methods
-   * 
-   * @param request
-   * @param response
-   * @throws ServletException
-   * @throws IOException
-   */
-  public void doService(HttpServletRequest request,
-      HttpServletResponse response) throws ServletException, IOException {
-    UseCase uc = MonitorFactory.startUseCase("RESTServlet(" + request.getServletPath() + ")");
-    log.debug("Request:" + request.getQueryString());
-    
-    // starttime
-    long s = new Date().getTime();
-    // get the objects
-    
-    HashMap<String,Resolvable> objects = new HashMap<String,Resolvable>();
-    objects.put("request", new RequestBeanWrapper(request));
-    objects.put("session", new HttpSessionWrapper(request.getSession()));
-    CRRequestBuilder rB = new CRRequestBuilder(request, crConf);
-    //response.setContentType(rB.getContentRepository(this.crConf.getEncoding()).getContentType()+"; charset="+this.crConf.getEncoding());
-    container.processService(rB, objects, response.getOutputStream(), new ServletResponseTypeSetter(response));
-    response.getOutputStream().flush();
-    response.getOutputStream().close();
-    // endtime
-    long e = new Date().getTime();
-    if(log.isInfoEnabled()) {
-      StringBuilder requestID = new StringBuilder();
-      requestID.append(request.getRequestURI());
-      if(request.getQueryString() != null) {
-        requestID.append('?');
-        requestID.append(request.getQueryString());
-      }
-      log.info("Executiontime for " + requestID + ":" + (e - s));
-    }
-    uc.stop();
-  }
+	}
 
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    doService(request, response);
-  }
+	@Override
+	public void destroy() {
+		if (this.container != null) {
+			this.container.finalize();
+		}
+	}
 
-  public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    doService(request, response);
-  }
+	/**
+	 * Wrapper Method for the doGet and doPost Methods.
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public void doService(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		UseCase uc = MonitorFactory.startUseCase("RESTServlet("
+				+ request.getServletPath() + ")");
+		log.debug("Request:" + request.getQueryString());
+
+		// starttime
+		long s = new Date().getTime();
+		// get the objects
+
+		HashMap<String, Resolvable> objects = new HashMap<String, Resolvable>();
+		objects.put("request", new RequestBeanWrapper(request));
+		objects.put("session", new HttpSessionWrapper(request.getSession()));
+		CRRequestBuilder rB = new CRRequestBuilder(request, crConf);
+		//response.setContentType(rB.getContentRepository(this.crConf.getEncoding()).getContentType()+"; charset="+this.crConf.getEncoding());
+		container.processService(rB, objects, response.getOutputStream(),
+				new ServletResponseTypeSetter(response));
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
+		// endtime
+		long e = new Date().getTime();
+		if (log.isInfoEnabled()) {
+			StringBuilder requestID = new StringBuilder();
+			requestID.append(request.getRequestURI());
+			if (request.getQueryString() != null) {
+				requestID.append('?');
+				requestID.append(request.getQueryString());
+			}
+			log.info("Executiontime for " + requestID + ":" + (e - s));
+		}
+		uc.stop();
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doService(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doService(request, response);
+	}
 
 }
