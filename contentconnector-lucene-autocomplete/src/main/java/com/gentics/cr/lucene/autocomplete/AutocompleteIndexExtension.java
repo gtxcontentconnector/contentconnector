8001df68786a50e5ba2ab85ea4ed8c1f340b2e1f
sebvogel <sebvogel@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-10-27 12:50:11 +0000

    created a IndexExtension for the autocompleter. the new AutocompleteIndexExtension uses the IndexJob to reindex the autcomplete index and the delete job the clear all documents from the index

---
+++ contentconnector-lucene-autocomplete/src/main/java/com/gentics/cr/lucene/autocomplete/AutocompleteIndexExtension.java
@@ -0,0 +1,243 @@
+package com.gentics.cr.lucene.autocomplete;
+
+import java.lang.reflect.Constructor;
+
+import org.apache.log4j.Logger;
+
+import com.gentics.cr.CRConfig;
+import com.gentics.cr.CRConfigUtil;
+import com.gentics.cr.configuration.GenericConfiguration;
+import com.gentics.cr.events.Event;
+import com.gentics.cr.events.EventManager;
+import com.gentics.cr.events.IEventReceiver;
+import com.gentics.cr.lucene.events.IndexingFinishedEvent;
+import com.gentics.cr.lucene.indexer.index.LuceneIndexLocation;
+import com.gentics.cr.util.indexing.AbstractIndexExtension;
+import com.gentics.cr.util.indexing.AbstractUpdateCheckerJob;
+import com.gentics.cr.util.indexing.IReIndexStrategy;
+import com.gentics.cr.util.indexing.IndexExtension;
+import com.gentics.cr.util.indexing.IndexLocation;
+import com.gentics.cr.util.indexing.ReIndexNoSkipStrategy;
+import com.gentics.cr.util.indexing.SimpleIndexJobAdderThread;
+
+/**
+ * This {@link IndexExtension} creates and maintains an autocomplete-index. The
+ * {@link Autocompleter} uses the index created by this extension to provide
+ * autocomplete search results.
+ * 
+ * 
+ * @author Sebastian Vogel <s.vogel@gentics.com>
+ */
+public class AutocompleteIndexExtension extends AbstractIndexExtension
+		implements IEventReceiver, AutocompleteConfigurationKeys {
+	protected static final Logger log = Logger
+			.getLogger(AutocompleteIndexExtension.class);
+
+	private static final String REINDEX_JOB = "reIndex";
+	private static final String CLEAR_JOB = "clearAutocompleteIndex";
+	private static final String[] jobs = { REINDEX_JOB, CLEAR_JOB };
+
+	private CRConfig config;
+	private LuceneIndexLocation source = null;
+	private LuceneIndexLocation autocompleteLocation;
+	private String autocompletefield = "content";
+
+	private IReIndexStrategy reindexStrategy;
+
+	private boolean autocompletereopenupdate = false;
+
+	private boolean subscribeToIndexFinished = false;
+
+	private IndexLocation callingIndexLocation;
+
+	/**
+	 * The constructor is called in the {@link IndexLocation}
+	 * 
+	 * @see AbstractIndexExtension#AbstractIndexExtension(CRConfig,
+	 *      IndexLocation)
+	 * @param config
+	 * @param callingLocation
+	 */
+	public AutocompleteIndexExtension(CRConfig config,
+			IndexLocation callingLocation) {
+		super(config, callingLocation);
+		this.config = config;
+		this.callingIndexLocation = callingLocation;
+
+		GenericConfiguration src_conf = (GenericConfiguration) config
+				.get(SOURCE_INDEX_KEY);
+		CRConfigUtil src_conf_util = new CRConfigUtil(src_conf,
+				"SOURCE_INDEX_KEY");
+		if (src_conf_util.getPropertySize() > 0) {
+			source = LuceneIndexLocation.getIndexLocation(src_conf_util);
+		}
+		if (source == null) {
+			source = (LuceneIndexLocation) callingLocation;
+		}
+
+		GenericConfiguration auto_conf = (GenericConfiguration) config
+				.get(AUTOCOMPLETE_INDEX_KEY);
+		autocompleteLocation = LuceneIndexLocation
+				.getIndexLocation(new CRConfigUtil(auto_conf,
+						AUTOCOMPLETE_INDEX_KEY));
+		autocompleteLocation.registerDirectoriesSpecial();
+
+		String s_autofield = config.getString(AUTOCOMPLETE_FIELD_KEY);
+		reindexStrategy = initReindexStrategy(config);
+		if (s_autofield != null)
+			this.autocompletefield = s_autofield;
+
+		autocompletereopenupdate = config.getBoolean(
+				AUTOCOMPLETE_REOPEN_UPDATE, autocompletereopenupdate);
+
+		subscribeToIndexFinished = config.getBoolean(
+				AUTOCOMPLETE_SUBSCRIBE_TO_INDEX_FINISHED,
+				subscribeToIndexFinished);
+
+		if (subscribeToIndexFinished) {
+			EventManager.getInstance().register(this);
+		}
+	}
+
+	/**
+	 * EventHandler: subscribes to the
+	 * {@link IndexingFinishedEvent#INDEXING_FINISHED_EVENT_TYPE}. <br>
+	 * If enabled in the config this method adds a reIndexing Job to the queue
+	 * of the {@link IndexLocation} which fired the event
+	 */
+	public void processEvent(Event event) {
+		if (subscribeToIndexFinished
+				&& IndexingFinishedEvent.INDEXING_FINISHED_EVENT_TYPE
+						.equals(event.getType())) {
+			IndexLocation il = (IndexLocation) event.getData();
+			if (!reindexStrategy.skipReIndex(il)) {
+				AbstractUpdateCheckerJob job = (AbstractUpdateCheckerJob) new AutocompleteIndexJob(
+						config, il, this);
+				SimpleIndexJobAdderThread thread = new SimpleIndexJobAdderThread(
+						il, job);
+				thread.start();
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.gentics.cr.util.indexing.AbstractIndexExtension#stop()
+	 */
+	@Override
+	public void stop() {
+		source.stop();
+		autocompleteLocation.stop();
+		if (subscribeToIndexFinished) {
+			EventManager.getInstance().unregister(this);
+		}
+	}
+
+	/**
+	 * Initialize a config class for the periodical execution flag of the
+	 * indexer. If init of the configured class fails, a fallback class is
+	 * returned.
+	 * 
+	 * @return configclass
+	 * @param config
+	 */
+	private IReIndexStrategy initReindexStrategy(final CRConfig config) {
+		String className = config.getString(REINDEXSTRATEGYCLASS_KEY);
+
+		if (className != null && className.length() != 0) {
+			try {
+				Class<?> clazz = Class.forName(className);
+				Constructor<?> constructor = clazz
+						.getConstructor(CRConfig.class);
+				return (IReIndexStrategy) constructor.newInstance(config);
+			} catch (Exception e) {
+				log.warn("Cound not init configured "
+						+ REINDEXSTRATEGYCLASS_KEY + ": " + className, e);
+			}
+		}
+		return new ReIndexNoSkipStrategy(config);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.gentics.cr.util.indexing.AbstractIndexExtension#addJob(java.lang.
+	 * String)
+	 */
+	@Override
+	public void addJob(String name) throws NoSuchMethodException {
+		addJob(name, null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.gentics.cr.util.indexing.AbstractIndexExtension#addJob(java.lang.
+	 * String, com.gentics.cr.util.indexing.IndexLocation)
+	 */
+	@Override
+	public void addJob(String name, IndexLocation indexLocation)
+			throws NoSuchMethodException {
+		IndexLocation actualLocation = callingIndexLocation;
+		if (indexLocation != null) {
+			actualLocation = indexLocation;
+		}
+		if (REINDEX_JOB.equalsIgnoreCase(name)) {
+			AbstractUpdateCheckerJob job = (AbstractUpdateCheckerJob) new AutocompleteIndexJob(
+					this.config, actualLocation, this);			
+			SimpleIndexJobAdderThread thread = new SimpleIndexJobAdderThread(
+					actualLocation, job);
+				thread.run();
+		} else if (CLEAR_JOB.equalsIgnoreCase(name)) {
+			AbstractUpdateCheckerJob job = (AbstractUpdateCheckerJob) new AutocompleteIndexDeleteJob(
+					this.config, actualLocation, this);
+			SimpleIndexJobAdderThread thread = new SimpleIndexJobAdderThread(
+					actualLocation, job);
+				thread.run();
+		} else {
+			throw new NoSuchMethodException("No Job-Method by the name: "
+					+ name);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.gentics.cr.util.indexing.AbstractIndexExtension#getJobs()
+	 */
+	@Override
+	public String[] getJobs() {
+		return jobs;
+	}
+
+	/**
+	 * get the {@link LuceneIndexLocation} that is used as source for the autocomplete index
+	 * @return the source index location
+	 */
+	public LuceneIndexLocation getSource() {
+		return source;
+	}
+
+	/**
+	 * get the {@link LuceneIndexLocation} where the autocomplete-index is
+	 * stored
+	 * 
+	 * @return the autocomplete index location
+	 */
+	public LuceneIndexLocation getAutocompleteLocation() {
+		return autocompleteLocation;
+	}
+
+	/**
+	 * get the field in the source index which should be used for autocomplete
+	 * 
+	 * @return the name of field (default: content)
+	 */
+	public String getAutocompletefield() {
+		return autocompletefield;
+	}
+
+}
---
+++ contentconnector-lucene-autocomplete/src/main/java/com/gentics/cr/lucene/autocomplete/AutocompleteIndexJob.java
@@ -0,0 +1,131 @@
+package com.gentics.cr.lucene.autocomplete;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.spell.LuceneDictionary;
+
+import com.gentics.cr.CRConfig;
+import com.gentics.cr.CRConfigUtil;
+import com.gentics.cr.exceptions.CRException;
+import com.gentics.cr.lucene.indexaccessor.IndexAccessor;
+import com.gentics.cr.lucene.indexer.index.LuceneIndexLocation;
+import com.gentics.cr.monitoring.MonitorFactory;
+import com.gentics.cr.monitoring.UseCase;
+import com.gentics.cr.util.indexing.AbstractUpdateCheckerJob;
+import com.gentics.cr.util.indexing.IndexLocation;
+
+
+/**
+ * This job is used to re-index (or newly index) the autocomplete-index 
+ * 
+ * @author Sebastian Vogel <s.vogel@gentics.com>
+ *
+ */
+public class AutocompleteIndexJob extends AbstractUpdateCheckerJob implements AutocompleteConfigurationKeys{
+
+	private AutocompleteIndexExtension autocompleter;
+	
+	public AutocompleteIndexJob(CRConfig updateCheckerConfig,
+			IndexLocation indexLoc,
+			AutocompleteIndexExtension autocompleter) {		
+		super(updateCheckerConfig, indexLoc, null);
+		
+		this.identifyer = identifyer.concat(":reIndex");
+		log = Logger.getLogger(AutocompleteIndexJob.class);
+		this.autocompleter = autocompleter;		
+	}
+
+	/**
+	 * starts the job - is called by the IndexJobQueue
+	 */
+	@Override
+	protected void indexCR(IndexLocation indexLocation, CRConfigUtil config)
+			throws CRException {		
+		try {
+			reIndex();
+		} catch (IOException e) {
+			throw new CRException("Could not access the Autocomplete index! " + e.getMessage());
+		}
+
+	}
+	
+	private synchronized void reIndex() throws IOException {
+		UseCase ucReIndex = MonitorFactory.startUseCase("reIndex()");
+		// build a dictionary (from the spell package)
+		log.debug("Starting to reindex autocomplete index.");
+		
+		LuceneIndexLocation source = this.autocompleter.getSource();
+		LuceneIndexLocation autocompleteLocation = this.autocompleter.getAutocompleteLocation();
+		String autocompletefield = this.autocompleter.getAutocompletefield();
+		
+		IndexAccessor sia = source.getAccessor();
+		IndexReader sourceReader = sia.getReader(false);
+		LuceneDictionary dict = new LuceneDictionary(sourceReader,
+				autocompletefield);
+		IndexAccessor aia = autocompleteLocation.getAccessor();
+		// IndexReader reader = aia.getReader(false);
+		IndexWriter writer = aia.getWriter();
+
+		try {
+			writer.setMergeFactor(300);
+			writer.setMaxBufferedDocs(150);
+			// go through every word, storing the original word (incl. n-grams)
+			// and the number of times it occurs
+			// CREATE WORD LIST FROM SOURCE INDEX
+			Map<String, Integer> wordsMap = new HashMap<String, Integer>();
+			Iterator<String> iter = (Iterator<String>) dict.getWordsIterator();
+			while (iter.hasNext()) {
+				String word = iter.next();
+				int len = word.length();
+				if (len < 3) {
+					continue; // too short we bail but "too long" is fine...
+				}
+				if (wordsMap.containsKey(word)) {
+					throw new IllegalStateException(
+							"Lucene returned a bad word list");
+				} else {
+					// use the number of documents this word appears in
+					wordsMap.put(word, sourceReader.docFreq(new Term(
+							autocompletefield, word)));
+				}
+			}
+			// DELETE OLD OBJECTS FROM INDEX
+			writer.deleteAll();
+
+			// UPDATE DOCUMENTS IN AUTOCOMPLETE INDEX
+			for (String word : wordsMap.keySet()) {
+				// ok index the word
+				Document doc = new Document();
+				doc.add(new Field(SOURCE_WORD_FIELD, word, Field.Store.YES,
+						Field.Index.NOT_ANALYZED_NO_NORMS)); // orig term
+				doc.add(new Field(GRAMMED_WORDS_FIELD, word, Field.Store.YES,
+						Field.Index.ANALYZED)); // grammed
+				doc.add(new Field(COUNT_FIELD, Integer.toString(wordsMap
+						.get(word)), Field.Store.YES,
+						Field.Index.NOT_ANALYZED_NO_NORMS)); // count
+				writer.addDocument(doc);
+			}
+			writer.optimize();
+		} finally {
+
+			sia.release(sourceReader, false);
+			// close writer
+
+			aia.release(writer);
+			// aia.release(reader,false);
+		}
+		autocompleteLocation.createReopenFile();
+		log.debug("Finished reindexing autocomplete index.");
+		ucReIndex.stop();
+	}
+
+}
