51e3004892c1a3ac757b232efbf9e59db353b36d
friesoft@gmail.com <friesoft@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-11-11 09:40:48 +0000

    unused imports

--- contentconnector-servlet/src/main/java/com/gentics/cr/servlet/VelocityServlet.java
+++ contentconnector-servlet/src/main/java/com/gentics/cr/servlet/VelocityServlet.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
