188e35192160a4c8ad132805c5ea7bf1e183a18e
friesoft@gmail.com <friesoft@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-11-11 10:51:58 +0000

    Updated CleanupTextTest to Junit 4, fixed exceptions by including a minimal config

--- contentconnector-lucene/src/test/java/com/gentics/cr/lucene/indexer/transformer/CleanupTextTest.java
+++ contentconnector-lucene/src/test/java/com/gentics/cr/lucene/indexer/transformer/CleanupTextTest.java
@@ -1,61 +1,93 @@
 package com.gentics.cr.lucene.indexer.transformer;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import com.gentics.cr.CRConfigUtil;
 import com.gentics.cr.CRResolvableBean;
 import com.gentics.cr.exceptions.CRException;
+import com.gentics.cr.util.CRUtil;
 
 import junit.framework.TestCase;
 
-public class CleanupTextTest extends TestCase {
-	
+public class CleanupTextTest {
+
 	private static final String CONTENT_ATTRIBUTE = "content";
-	
+
 	private static final String UMLAUTS = "öäüÄÜÖß€";
-	
+
 	private ContentTransformer transformer;
-	@Override
-	protected void setUp() throws Exception {
+
+	@Before
+	public void setUp() throws Exception {
+		URL confPath = new File(this.getClass().getResource(
+				"/config/nodelog.properties").toURI()).getParentFile().toURI()
+				.toURL();
+		System.setProperty(CRUtil.PORTALNODE_CONFPATH, confPath.getPath());
 		CRConfigUtil config = new CRConfigUtil();
 		config.set("attribute", CONTENT_ATTRIBUTE);
 		transformer = new CleanupText(config);
 	}
-	
+
+	@Test
 	public void testIndexPoints() throws CRException {
-		String result = transform("1. Index\n" + 
-				"Title ....................................................................................................................... 1\n" +
-				"1. First chapter ...................................................................................................................... 2\n" +
-				"2. Second chapter .................................................................................................................................... 7");
-		assertEquals("Index points are not reduced correctly.", "1. Index Title ... 1 1. First chapter ... 2 2. Second chapter ... 7", result);
-		
+		String result = transform("1. Index\n"
+				+ "Title ....................................................................................................................... 1\n"
+				+ "1. First chapter ...................................................................................................................... 2\n"
+				+ "2. Second chapter .................................................................................................................................... 7");
+		assertEquals(
+				"Index points are not reduced correctly.",
+				"1. Index Title ... 1 1. First chapter ... 2 2. Second chapter ... 7",
+				result);
+
 		result = transform("First chapter . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . Page 33 Second chapter . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . Page 66");
-		assertEquals("Index points are not reduced correctly.", "First chapter ... Page 33 Second chapter ... Page 66", result);
+		assertEquals("Index points are not reduced correctly.",
+				"First chapter ... Page 33 Second chapter ... Page 66", result);
 	}
-	
+
+	@Test
 	public void testUmlauts() throws CRException {
-		assertEquals("Umlauts cannot be processed correctly.", UMLAUTS, transform(UMLAUTS + ""));
+		assertEquals("Umlauts cannot be processed correctly.", UMLAUTS,
+				transform(UMLAUTS + ""));
 	}
-	
+
+	@Test
 	public void testNotPrintableCharacters() throws CRException {
 		String stringWithNonPrintableCharacters = "Drittstaaten:  HYPERLINK \"http://www.help.gv.at/Content.Node/12/Seite.120000.html\" \\o \"Öffnet in neuem Fenster\" \\t \"_blank\" Aufenthaltsberechtigung";
 		String expectedResult = "Drittstaaten: HYPERLINK \"http://www.help.gv.at/Content.Node/12/Seite.120000.html\" \\o \"Öffnet in neuem Fenster\" \\t \"_blank\" Aufenthaltsberechtigung";
-		assertEquals("Special characters are not elminiated correctly.", expectedResult, transform(stringWithNonPrintableCharacters + ""));
-		
+		assertEquals("Special characters are not elminiated correctly.",
+				expectedResult,
+				transform(stringWithNonPrintableCharacters + ""));
+
 		stringWithNonPrintableCharacters = "Person Familienname:  FORMTEXT       Vorname:  FORMTEXT       Standort:  FORMTEXT       Stock:  FORMTEXT ";
 		expectedResult = "Person Familienname: FORMTEXT Vorname: FORMTEXT Standort: FORMTEXT Stock: FORMTEXT ";
-		assertEquals("Special characters are not elminiated correctly.", expectedResult, transform(stringWithNonPrintableCharacters + ""));
+		assertEquals("Special characters are not elminiated correctly.",
+				expectedResult,
+				transform(stringWithNonPrintableCharacters + ""));
 	}
-	
+
+	@Test
 	public void testMultipleSpaces() throws CRException {
 		final String stringWithMultipleSpaces = "test1  test2   test3    test4";
 		final String expectedResult = "test1 test2 test3 test4";
-		assertEquals("Multiple Spaces are not replaced correctly.", expectedResult, transform(stringWithMultipleSpaces));
+		assertEquals("Multiple Spaces are not replaced correctly.",
+				expectedResult, transform(stringWithMultipleSpaces));
 	}
-	
+
+	@Test
 	public void testPendingSpace() throws CRException {
-		assertEquals("Pending Space was not handled correctly.", "a ", transform("a "));
-		assertEquals("Pending Space was not handled correctly.", "a... ", transform("a.... "));
+		assertEquals("Pending Space was not handled correctly.", "a ",
+				transform("a "));
+		assertEquals("Pending Space was not handled correctly.", "a... ",
+				transform("a.... "));
 	}
-	
+
+	@Test
 	public void testByteArray() throws CRException {
 		CRResolvableBean bean = new CRResolvableBean();
 		bean.set(CONTENT_ATTRIBUTE, UMLAUTS.getBytes());
@@ -63,11 +95,13 @@ public class CleanupTextTest extends TestCase {
 		String result = bean.getString(CONTENT_ATTRIBUTE);
 		assertEquals("Cannot handle the byte array correctly.", UMLAUTS, result);
 	}
-	
+
+	@Test
 	public void testUnchanged() throws CRException {
 		String testStringNotToChange = "test";
-		assertSame("String had not to be changed.", testStringNotToChange, transform(testStringNotToChange));
-		
+		assertSame("String had not to be changed.", testStringNotToChange,
+				transform(testStringNotToChange));
+
 	}
 
 	private String transform(String string) throws CRException {
