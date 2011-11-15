a871ba2677c99085a6673ef1982a390b8336b9ef
friesoft@gmail.com <friesoft@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-11-15 11:33:05 +0000

    added method to remove empty clauses from a BooleanQuery (those lead to QueryParser ParseExceptions) adapted javadoc of the calling functions with a hint that empty clauses will be removed

--- contentconnector-lucene/src/main/java/com/gentics/cr/lucene/search/query/BooleanQueryRewriter.java
+++ contentconnector-lucene/src/main/java/com/gentics/cr/lucene/search/query/BooleanQueryRewriter.java
@@ -11,7 +11,7 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 
 public class BooleanQueryRewriter {
-	
+
 	/**
 	 * Log4j logger for error and debug messages.
 	 */
@@ -20,10 +20,11 @@ public class BooleanQueryRewriter {
 	/**
 	 * private constructor to prevents generating an instance.
 	 */
-	private BooleanQueryRewriter() { }
+	private BooleanQueryRewriter() {
+	}
 
 	/**
-	 * Replace all occurences of the needle with the replacement in the query.
+	 * Replace all occurences of the needle with the replacement in the query; cleans query of empty clauses.
 	 * @param query - lucene query to rewrite
 	 * @param needle - lucene term to replace
 	 * @param replacement - replacement term to replace the needle in the query.
@@ -36,7 +37,7 @@ public class BooleanQueryRewriter {
 	}
 
 	/**
-	 * Replace all occurences of the given terms in the query.
+	 * Replace all occurences of the given terms in the query; cleans query of empty clauses.
 	 * @param query - lucene query to rewrite
 	 * @param replacements - map consisting of entries of terms, the key is the term to replace,
 	 * the value is the replacement.
@@ -50,7 +51,7 @@ public class BooleanQueryRewriter {
 			LOGGER.debug("\tin " + query);
 		}
 		if (query instanceof TermQuery) {
-			for(Term origTerm : replacements.keySet()) {
+			for (Term origTerm : replacements.keySet()) {
 				if (((TermQuery) query).getTerm().compareTo(origTerm) == 0) {
 					return new TermQuery(replacements.get(origTerm));
 				}
@@ -58,12 +59,46 @@ public class BooleanQueryRewriter {
 		} else if (query instanceof BooleanQuery) {
 			BooleanQuery newQuery = new BooleanQuery();
 			for (BooleanClause clause : ((BooleanQuery) query).getClauses()) {
-				newQuery.add(new BooleanClause(replaceTerms(clause.getQuery(), replacements), clause.getOccur()));
+				// recursive function call to handle all terms of the query
+				Query replacedTerms = replaceTerms(clause.getQuery(), replacements);
+
+				// we need to check if the query is null because 
+				// removeEmptyClauses returns null if there are no clauses in it (except empty ones)
+				if (replacedTerms != null) {
+					BooleanClause newClause = new BooleanClause(replacedTerms, clause.getOccur());
+					newQuery.add(newClause);
+				}
 			}
+			// if the query contains empty clauses we want them removed
+			// if the query does not contain any non empty clauses this method returns null
+			newQuery = removeEmptyClauses(newQuery);
 			return newQuery;
 		} else {
 			LOGGER.error("Cannot rewrite query '" + query + "' because it is an unkonwn type of query.");
 		}
 		return query;
+	}
+
+	/**
+	 * Remove all empty clauses from the query.
+	 * @param query Query to be cleaned up
+	 * @return Null if the query only consists of empty clauses or the cleaned up query.
+	 */
+	public static BooleanQuery removeEmptyClauses(final BooleanQuery query) {
+		BooleanQuery cleanedQuery = new BooleanQuery();
+
+		BooleanClause[] clauses = query.getClauses();
+		int clauseCount = 0;
+		for (BooleanClause clause : clauses) {
+			if (clause != null && !clause.toString().equals("")) {
+				cleanedQuery.add(clause);
+				clauseCount++;
+			}
+		}
+		if (clauseCount == 0) {
+			return null;
+		} else {
+			return cleanedQuery;
+		}
 	}
 }
