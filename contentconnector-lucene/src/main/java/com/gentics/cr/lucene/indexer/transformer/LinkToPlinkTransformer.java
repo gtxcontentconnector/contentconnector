2c3f9a25c9052b6064fc8021d93095503bfc6d6a
friesoft@gmail.com <friesoft@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-11-16 08:35:10 +0000

    Coding style

--- contentconnector-lucene/src/main/java/com/gentics/cr/lucene/indexer/transformer/LinkToPlinkTransformer.java
+++ contentconnector-lucene/src/main/java/com/gentics/cr/lucene/indexer/transformer/LinkToPlinkTransformer.java
@@ -12,6 +12,7 @@ import com.gentics.cr.CRResolvableBean;
 import com.gentics.cr.configuration.GenericConfiguration;
 import com.gentics.cr.exceptions.CRException;
 import com.gentics.cr.plink.LucenePathResolver;
+
 /**
  * 
  * Last changed: $Date: 2009-06-24 17:10:19 +0200 (Mi, 24 Jun 2009) $
@@ -21,119 +22,109 @@ import com.gentics.cr.plink.LucenePathResolver;
  */
 public class LinkToPlinkTransformer extends ContentTransformer {
 	private static Logger log = Logger.getLogger(LinkToPlinkTransformer.class);
-	private static final String ATTRIBUTE_KEY="attribute";
-	private static final String STATIC_PREFIX_KEY="staticprefix";
-	private static final String HOST_PREFIX_KEY="hostprefix";
-	
-	
-	private String attribute="content";
+	private static final String ATTRIBUTE_KEY = "attribute";
+	private static final String STATIC_PREFIX_KEY = "staticprefix";
+	private static final String HOST_PREFIX_KEY = "hostprefix";
+
+	private String attribute = "content";
 	Pattern plinkResolverPattern = Pattern.compile("(href|src)=\"([^\"]+)\"");
-	Pattern parameterPattern = Pattern.compile("(#|\\?)(.*)"); 
-	Pattern excludeHostPattern = Pattern.compile("^([a-zA-Z][a-zA-Z0-9^:]*:|#|<plink)");
+	Pattern parameterPattern = Pattern.compile("(#|\\?)(.*)");
+	Pattern excludeHostPattern = Pattern
+			.compile("^([a-zA-Z][a-zA-Z0-9^:]*:|#|<plink)");
 
 	CRConfigUtil config;
 	LucenePathResolver pr;
-	String staticprefix=null;
+	String staticprefix = null;
 	String hostprefix = null;
+
 	/**
-	 * Create new Instance
+	 * Create new Instance.
 	 * @param config
 	 */
 	public LinkToPlinkTransformer(GenericConfiguration config) {
 		super(config);
-		this.config=new CRConfigUtil(config,"link_to_plinktransformer");
-		String att_string = config.getString(ATTRIBUTE_KEY);
-		if(att_string != null)
-		{
-			this.attribute = att_string;
+		this.config = new CRConfigUtil(config, "link_to_plinktransformer");
+		String attString = config.getString(ATTRIBUTE_KEY);
+		if (attString != null) {
+			this.attribute = attString;
 		}
 		staticprefix = this.config.getString(STATIC_PREFIX_KEY);
 		hostprefix = this.config.getString(HOST_PREFIX_KEY);
-		
+
 		pr = new LucenePathResolver(this.config);
-		
-		
+
 	}
 
 	@Override
 	public void processBean(CRResolvableBean bean) throws CRException {
-		String content = (String)bean.get(this.attribute);
-		if(content!=null)
-		{
+		String content = (String) bean.get(this.attribute);
+		if (content != null) {
 			// starttime
 			long s = new Date().getTime();
 			String replacement = resolveLinks(content);
 			bean.set(this.attribute, replacement);
 			long e = new Date().getTime();
-			log.debug("Resolving static URLs took " + (e - s)+"ms");
+			log.debug("Resolving static URLs took " + (e - s) + "ms");
 		}
 	}
-	
-	
-	
+
 	private String resolveLinks(String content) {
-		  Matcher matcher = plinkResolverPattern.matcher(content);
-		  StringBuffer buf = new StringBuffer();
-		  while (matcher.find()) {
-			  String linkform = matcher.group(1);
-			  String link = matcher.group(2);
-			  String staticlink = link;
-			  if(this.staticprefix!=null)
-			  {
-				  link = link.replaceFirst(this.staticprefix, "");
-			  }
-			  
-			  //EXTRACT PARAMETERS
-			  Matcher parammatcher = parameterPattern.matcher(link);
-			  String parameters = "";
-			  if(parammatcher.find())
-			  {
-				  StringBuffer lp_buff = new StringBuffer();
-				  parameters = parammatcher.group(1)+parammatcher.group(2);
-				  parammatcher.appendReplacement(lp_buff, "");
-				  parammatcher.appendTail(lp_buff);
-				  link = lp_buff.toString();
-			  }
-			  
-			  //RESOLVE CONTENTID
-			  CRResolvableBean bean = null;
-			  if(link!=null&&!"".equals(link)&&!link.startsWith("http://"))
-			  {
-				  CRRequest r = new CRRequest();
-				  r.setUrl(link);
-				  bean = this.pr.getObject(r);
-				  if(bean!=null)
-				  {
-					  String contentid = bean.getContentid();
-					  link = "<plink id=\""+contentid+"\">";
-				  }
-				  else
-				  {
-					  log.warn("Could not resolve internal link: "+link);
-				  }
-				  log.warn("would be resolving");
-			  }
-			  
-			  //ADD PARAMETERS
-			  link+=parameters;
-			  //PREFIX HOST
-			  Matcher host_m = excludeHostPattern.matcher(link);
-			  if(!host_m.find())
-			  {
-				  link = this.hostprefix+staticlink;
-			  }
-			  //REAPPEND FINISHED LINK
-			  //we need to escape $ as this kills the expression
-			  matcher.appendReplacement(buf, linkform+"=\""+link.replace("$", "\\$") + "\"");
-		  }
-		  matcher.appendTail(buf);
-		  return buf.toString();
+		Matcher matcher = plinkResolverPattern.matcher(content);
+		StringBuffer buf = new StringBuffer();
+		while (matcher.find()) {
+			String linkform = matcher.group(1);
+			String link = matcher.group(2);
+			String staticlink = link;
+			if (this.staticprefix != null) {
+				link = link.replaceFirst(this.staticprefix, "");
+			}
+
+			//EXTRACT PARAMETERS
+			Matcher parammatcher = parameterPattern.matcher(link);
+			String parameters = "";
+			if (parammatcher.find()) {
+				StringBuffer lpBuff = new StringBuffer();
+				parameters = parammatcher.group(1) + parammatcher.group(2);
+				parammatcher.appendReplacement(lpBuff, "");
+				parammatcher.appendTail(lpBuff);
+				link = lpBuff.toString();
+			}
+
+			//RESOLVE CONTENTID
+			CRResolvableBean bean = null;
+			if (link != null && !"".equals(link) && !link.startsWith("http://")) {
+				CRRequest r = new CRRequest();
+				r.setUrl(link);
+				bean = this.pr.getObject(r);
+				if (bean != null) {
+					String contentid = bean.getContentid();
+					link = "<plink id=\"" + contentid + "\">";
+				} else {
+					log.warn("Could not resolve internal link: " + link);
+				}
+				log.warn("would be resolving");
+			}
+
+			//ADD PARAMETERS
+			link += parameters;
+			//PREFIX HOST
+			Matcher hostMatcher = excludeHostPattern.matcher(link);
+			if (!hostMatcher.find()) {
+				link = this.hostprefix + staticlink;
+			}
+			//REAPPEND FINISHED LINK
+			//we need to escape $ as this kills the expression
+			matcher.appendReplacement(buf, linkform + "=\""
+					+ link.replace("$", "\\$") + "\"");
+		}
+		matcher.appendTail(buf);
+		return buf.toString();
 	}
 
 	@Override
 	public void destroy() {
 		this.pr.destroy();
-		
+
 	}
 
 }
