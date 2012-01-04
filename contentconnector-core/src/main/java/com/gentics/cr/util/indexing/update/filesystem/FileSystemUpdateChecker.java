95b621fee1b4e94ba7a3f5c6a6f21d0b9fc16fd0
bigbear.ap@gmail.com <bigbear.ap@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2012-01-04 08:57:49 +0000

    added file system indexing tools used to write changes of the content repository into the filesystem

---
+++ contentconnector-core/src/main/java/com/gentics/cr/util/indexing/update/filesystem/FileSystemUpdateChecker.java
@@ -0,0 +1,66 @@
+package com.gentics.cr.util.indexing.update.filesystem;
+
+import java.io.File;
+
+import com.gentics.api.lib.resolving.Resolvable;
+import com.gentics.cr.CRResolvableBean;
+import com.gentics.cr.configuration.GenericConfiguration;
+import com.gentics.cr.exceptions.CRException;
+import com.gentics.cr.util.AccessibleBean;
+import com.gentics.cr.util.indexing.IndexUpdateChecker;
+
+public class FileSystemUpdateChecker extends IndexUpdateChecker {
+
+	File directory;
+	
+	boolean ignorePubDir;
+	
+	public FileSystemUpdateChecker(GenericConfiguration config) {
+		directory = new File(config.getString("directory"));
+		ignorePubDir = config.getBoolean("ignorePubDir");
+	}
+
+	@Override
+	protected boolean checkUpToDate(String identifyer, Object timestamp,
+			String timestampattribute, Resolvable object) {
+		CRResolvableBean bean = new CRResolvableBean(object);
+		if(!"10002".equals(bean.getObj_type())) {
+			String publicationDirectory;
+			if (ignorePubDir) {
+				publicationDirectory = "";
+			} else {
+				publicationDirectory = bean.getString("pub_dir");
+			}
+			String filename = bean.getString("filename");
+			assertNotNull("Bean " + bean.getContentid() + " has no attribute pub_dir.", publicationDirectory);
+			assertNotNull("Bean " + bean.getContentid() + " has no attribute filename.", filename);
+			Integer updatetimestamp = (Integer) timestamp;
+			File file = new File(new File(directory, publicationDirectory), filename);
+			if(file.exists() && file.isFile() && (file.lastModified() / 1000) >= updatetimestamp) {
+				return true;
+			}
+		} else if(!ignorePubDir) {
+			//it would just make no sense to check for check for folders existence if the pub_dir attribute is ignored
+			String publicationDirectory = bean.getString("pub_dir");
+			File file = new File(directory, publicationDirectory);
+			if (file.exists() && file.isDirectory()) {
+				return true;
+			}
+		} else {
+			return true;
+		}
+		return false;
+	}
+
+	protected static void assertNotNull(String message, Object object) {
+		if (object == null) {
+			throw new RuntimeException(new CRException(message));
+		}
+	}
+
+	@Override
+	public void deleteStaleObjects() {
+		// TODO Auto
+	}
+
+}
---
+++ contentconnector-core/src/main/java/com/gentics/cr/util/indexing/update/filesystem/FileSystemUpdateJob.java
@@ -0,0 +1,105 @@
+package com.gentics.cr.util.indexing.update.filesystem;
+
+import static com.gentics.cr.util.indexing.update.filesystem.FileSystemUpdateChecker.assertNotNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Hashtable;
+
+import com.gentics.cr.CRConfig;
+import com.gentics.cr.CRConfigUtil;
+import com.gentics.cr.CRRequest;
+import com.gentics.cr.CRResolvableBean;
+import com.gentics.cr.RequestProcessor;
+import com.gentics.cr.exceptions.CRException;
+import com.gentics.cr.util.indexing.AbstractUpdateCheckerJob;
+import com.gentics.cr.util.indexing.IndexLocation;
+
+public class FileSystemUpdateJob extends AbstractUpdateCheckerJob {
+
+	File directory;
+	
+	boolean ignorePubDir;
+	
+	public FileSystemUpdateJob(CRConfig config,
+			IndexLocation indexLoc,
+			Hashtable<String, CRConfigUtil> updateCheckerConfigmap) {
+		super(config, indexLoc, updateCheckerConfigmap);
+		
+		try {
+			rp = config.getNewRequestProcessorInstance(1);
+		} catch (CRException e) {
+			log.error("Could not create RequestProcessor instance." 
+					+ config.getName(), e);
+		}
+		
+		indexUpdateChecker = new FileSystemUpdateChecker(config.getSubConfig("updatejob"));
+		ignorePubDir = config.getBoolean("updatejob.ignorePubDir");
+		directory = new File(config.getString("updatejob.directory"));
+	}
+	
+	RequestProcessor rp;
+	
+	FileSystemUpdateChecker indexUpdateChecker;
+	
+	
+
+	@Override
+	protected void indexCR(IndexLocation indexLocation, CRConfigUtil config)
+			throws CRException {
+		Collection<CRResolvableBean> objectsToIndex = null;
+		try {
+			CRRequest req = new CRRequest();
+			req.setRequestFilter("1==1");
+			status.setCurrentStatusString("Get objects to update in the index ...");
+				objectsToIndex = getObjectsToUpdate(req, rp, false, indexUpdateChecker);
+			} catch (Exception e) {
+				log.error("ERROR while cleaning index", e);
+			}
+			
+			for(CRResolvableBean bean : objectsToIndex) {
+				if(!"10002".equals(bean.getObj_type())) {
+					String publicationDirectory;
+					if (ignorePubDir) {
+						publicationDirectory = "";
+					} else {
+						publicationDirectory = bean.getString("pub_dir");
+					}
+					String filename = bean.getString("filename");
+					assertNotNull("Bean " + bean.getContentid() + " has no attribute pub_dir.", publicationDirectory);
+					assertNotNull("Bean " + bean.getContentid() + " has no attribute filename.", filename);
+					File file = new File(new File(directory, publicationDirectory), filename);
+					if(file.isDirectory()) {
+						file.delete();
+					}
+					try {
+						FileWriter writer = new FileWriter(file);
+						String contentAttribute;
+						if("10007".equals(bean.getObj_type())) {
+							contentAttribute = "content";
+						} else {
+							contentAttribute = "binarycontent";
+						}
+						writer.write(bean.getString(contentAttribute));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				} else if(!ignorePubDir) {
+					//it would just make no sense to check for check for folders existence if the pub_dir attribute is ignored
+					String publicationDirectory = bean.getString("pub_dir");
+					File file = new File(directory, publicationDirectory);
+					file.mkdirs();
+				}
+			}
+
+	}
+
+}
