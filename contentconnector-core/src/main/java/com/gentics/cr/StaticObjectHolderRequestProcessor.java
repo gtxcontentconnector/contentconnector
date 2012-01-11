c3bdf132fea8e1a5f2665fbc9e827773a32ac9d1
bigbear.ap@gmail.com <bigbear.ap@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2012-01-11 14:55:52 +0000

    added a simple requestprocessor that just holds a collection of objects

---
+++ contentconnector-core/src/main/java/com/gentics/cr/StaticObjectHolderRequestProcessor.java
@@ -0,0 +1,49 @@
+package com.gentics.cr;
+
+import java.util.Collection;
+import java.util.Vector;
+
+import com.gentics.cr.exceptions.CRException;
+
+/**
+ * This is a simple RequestProcessor that just holds a static array of CRResolvables in the JVM. This is especially useful for unit testing classes that work with a RequestProcessor but maybe somebody can use this for other things.
+ * @author bigbear3001
+ *
+ */
+public class StaticObjectHolderRequestProcessor extends RequestProcessor {
+
+	/**
+	 * Collection of resolvables that we return in {@link #getObjects(CRRequest, boolean)}
+	 */
+	public static Collection<CRResolvableBean> objects = new Vector<CRResolvableBean>();
+	
+	/**
+	 * Initialize a new StaticObjectHolderRequestProcessor
+	 * @param config - configuration of the RequestProcessor
+	 * @throws CRException - if no configuration is given, or the PlinkProcessor could not
+	 * be initialized.
+	 * @see {@link RequestProcessor} as we just put the configuration thru
+	 */
+	public StaticObjectHolderRequestProcessor(CRConfig config)
+			throws CRException {
+		super(config);
+	}
+
+	@Override
+	public Collection<CRResolvableBean> getObjects(CRRequest request,
+			boolean doNavigation) throws CRException {
+		//TODO add the possibility to filter the objects with the request
+		return objects;
+	}
+
+	@Override
+	public void finalize() { }
+	
+	/**
+	 * @param newObjects - collection of resolvables that should be returned, as this one is used in all threads it should be thread safe if you plan to manipulate in any way.
+	 */
+	public static void setObjects(Collection<CRResolvableBean> newObjects) {
+		objects = newObjects;
+	}
+
+}
