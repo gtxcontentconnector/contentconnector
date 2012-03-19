a544d776a1912154b393a8985baef50f2a55317f
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2012-03-19 13:08:37 +0000

    fixed exception when requesting binarycontent

--- contentconnector/contentconnector-json/src/main/java/com/gentics/cr/rest/json/JSONContentRepository.java
+++ contentconnector/contentconnector-json/src/main/java/com/gentics/cr/rest/json/JSONContentRepository.java
@@ -18,12 +18,13 @@ import com.gentics.cr.rest.ContentRepository;
 
 /**
  * Implementaion of Json rappresentation for a REST contentrepositroy.
- *
+ * 
  * 
  * Last changed: $Date: 2010-04-01 15:25:54 +0200 (Do, 01 Apr 2010) $
+ * 
  * @version $Revision: 545 $
  * @author $Author: supnig@constantinopel.at $
- *
+ * 
  */
 public class JSONContentRepository extends ContentRepository {
 
@@ -32,8 +33,8 @@ public class JSONContentRepository extends ContentRepository {
 	private JSONObject rootObject;
 
 	/**
-	 * Create instance
-	 * 	Set response encoding to UTF-8
+	 * Create instance Set response encoding to UTF-8
+	 * 
 	 * @param attr
 	 */
 	public JSONContentRepository(String[] attr) {
@@ -42,9 +43,10 @@ public class JSONContentRepository extends ContentRepository {
 		this.setResponseEncoding("UTF-8");
 		rootObject = new JSONObject();
 	}
-	
+
 	/**
 	 * Create instance
+	 * 
 	 * @param attr
 	 * @param encoding
 	 */
@@ -54,103 +56,104 @@ public class JSONContentRepository extends ContentRepository {
 		this.setResponseEncoding(encoding);
 		rootObject = new JSONObject();
 	}
-	
+
 	/**
 	 * Create instance
+	 * 
 	 * @param attr
 	 * @param encoding
 	 * @param options
 	 */
-	public JSONContentRepository(String[] attr, String encoding, String[] options) {
+	public JSONContentRepository(String[] attr, String encoding,
+			String[] options) {
 
-		super(attr,encoding,options);
-		//this.setResponseEncoding(encoding);
+		super(attr, encoding, options);
+		// this.setResponseEncoding(encoding);
 		rootObject = new JSONObject();
 	}
 
 	/**
 	 * 
 	 * Returns "application/jsonrequest"
-	 * @return 
+	 * 
+	 * @return
 	 * 
 	 */
 	public String getContentType() {
-		//return "text/javascript";
-		return("application/jsonrequest");
+		// return "text/javascript";
+		return ("application/jsonrequest");
 	}
-	
+
 	/**
 	 * Responds with a JSON ERROR OBject
-	 * @param stream 
-	 * @param ex 
-	 * @param isDebug 
+	 * 
+	 * @param stream
+	 * @param ex
+	 * @param isDebug
 	 * 
 	 */
-	public void respondWithError(OutputStream stream,CRException ex, boolean isDebug)
-	{
-		//Clear the root element and set the status
+	public void respondWithError(OutputStream stream, CRException ex,
+			boolean isDebug) {
+		// Clear the root element and set the status
 		this.rootObject.clear();
-		this.rootObject.element("status","error");
+		this.rootObject.element("status", "error");
 		JSONObject errElement = new JSONObject();
-		
-		errElement.element("type",ex.getType());
-		errElement.element("message",ex.getMessage());
-		if(isDebug)
-		{
-			//if in debug mode, add stack trace
-			errElement.element("stacktrace",ex.getStringStackTrace());
+
+		errElement.element("type", ex.getType());
+		errElement.element("message", ex.getMessage());
+		if (isDebug) {
+			// if in debug mode, add stack trace
+			errElement.element("stacktrace", ex.getStringStackTrace());
 		}
-		this.rootObject.element("Error",errElement);
-		
-		//Write stream 
+		this.rootObject.element("Error", errElement);
+
+		// Write stream
 		try {
 			stream.write(this.rootObject.toString().getBytes());
 		} catch (IOException e) {
 			;
 		}
-		
+
 	}
-	
+
 	/**
 	 * Writes the objects as JSON Stream
-	 * @param stream 
-	 * @throws CRException 
+	 * 
+	 * @param stream
+	 * @throws CRException
 	 * 
 	 */
-	public void toStream(OutputStream stream) throws CRException{
-		if(this.resolvableColl.isEmpty())
-		{
-			//No Data Found
-			throw new CRException("NoDataFound","Data could not be found.");
-		}
-		else
-		{
-			this.rootObject.element("status","ok");
-			
+	public void toStream(OutputStream stream) throws CRException {
+		if (this.resolvableColl.isEmpty()) {
+			// No Data Found
+			throw new CRException("NoDataFound", "Data could not be found.");
+		} else {
+			this.rootObject.element("status", "ok");
+
 			JSONArray jsArray = new JSONArray();
-			
-			for (Iterator<CRResolvableBean> it = this.resolvableColl.iterator(); it.hasNext();) {
-	
-				CRResolvableBean crBean =  it.next();
-	
+
+			for (Iterator<CRResolvableBean> it = this.resolvableColl.iterator(); it
+					.hasNext();) {
+
+				CRResolvableBean crBean = it.next();
+
 				JSONObject objElement = processElement(crBean);
-	
+
 				jsArray.add(objElement);
-				
+
 			}
-			this.rootObject.element("Objects",jsArray);
+			this.rootObject.element("Objects", jsArray);
 		}
 
-		try
-		{
+		try {
 			// use JSONObject.write instead toString is not a good solution
-			OutputStreamWriter wr = new OutputStreamWriter(stream, this.getResponseEncoding());
-			
+			OutputStreamWriter wr = new OutputStreamWriter(stream,
+					this.getResponseEncoding());
+
 			this.rootObject.write(wr);
 			wr.flush();
 			wr.close();
-		}catch(IOException ioex)
-		{
+		} catch (IOException ioex) {
 			;
 		}
 
@@ -163,94 +166,101 @@ public class JSONContentRepository extends ContentRepository {
 		objElement.element("contentid", "" + crBean.getContentid());
 		objElement.element("obj_id", "" + crBean.getObj_id());
 		objElement.element("obj_type", "" + crBean.getObj_type());
-		objElement.element("mother_id",
-				((crBean.getMother_id() == null) ? "" : ""
-						+ crBean.getMother_id()));
-		objElement.element("mother_type",
+		objElement.element("mother_id", ((crBean.getMother_id() == null) ? ""
+				: "" + crBean.getMother_id()));
+		objElement.element(
+				"mother_type",
 				((crBean.getMother_type() == null) ? "" : ""
 						+ crBean.getMother_type()));
 
 		if (crBean.getAttrMap() != null && (!crBean.getAttrMap().isEmpty())) {
 			JSONObject attrContainer = new JSONObject();
-			
+
 			Iterator<String> bit = crBean.getAttrMap().keySet().iterator();
 			while (bit.hasNext()) {
 
 				String entry = bit.next();
 				// Element attrElement = doc.createElement(entry);
-				Object bValue=crBean.getAttrMap().get(entry);
-				
-				if(bValue!=null)
-				{
-					//deal with multivalue attributes
-					if(bValue.getClass().isArray()|| bValue instanceof List)
-					{
+				Object bValue = crBean.getAttrMap().get(entry);
+
+				if (bValue != null) {
+					// deal with multivalue attributes
+					if ((bValue.getClass().isArray() && !(bValue instanceof byte[]))
+							|| bValue instanceof List) {
 						JSONArray value = new JSONArray();
 						List<Object> arr;
-						if(bValue instanceof List) {
+						if (bValue instanceof List) {
 							arr = new ArrayList<Object>();
 							List<Object> ob_arr = (List<Object>) bValue;
-							for(Object obj : ob_arr) {								
-								String v ="";
+							for (Object obj : ob_arr) {
+								String v = "";
 								v = convertToString(obj);
 								arr.add(v);
 							}
 						} else {
 							arr = new ArrayList<Object>();
-							Object[] ob_arr = (Object[])bValue;
-							for(Object obj : ob_arr) {								
-								String v ="";
+							Object[] ob_arr = (Object[]) bValue;
+							for (Object obj : ob_arr) {
+								String v = "";
 								v = convertToString(obj);
 								arr.add(v);
 							}
 						}
 						value.addAll(arr);
 						attrContainer.element(entry, value);
-					} else if(bValue instanceof Map<?, ?>) {
+					} else if (bValue instanceof Map<?, ?>) {
 						attrContainer.element(entry, bValue);
 					}
-					//dealt with single value attributes (String, Number, ByteArray)
-					else
-					{
-						String value="";				
-						value = convertToString(bValue);						
+					// dealt with single value attributes (String, Number,
+					// ByteArray)
+					else {
+						String value = "";
+						value = convertToString(bValue);
 						attrContainer.element(entry, value);
-					}					
-				}				
+					}
+				}
 			}
-			objElement.element("attributes",attrContainer);
+			objElement.element("attributes", attrContainer);
 		}
-		if(crBean.getChildRepository() != null && crBean.getChildRepository().size()>0)
-		{
-			//JSONObject childContainer = new JSONObject();
+		if (crBean.getChildRepository() != null
+				&& crBean.getChildRepository().size() > 0) {
+			// JSONObject childContainer = new JSONObject();
 			JSONArray childContainer = new JSONArray();
-			for (Iterator it = crBean.getChildRepository().iterator(); it.hasNext();) {
-				
+			for (Iterator it = crBean
+					.getChildRepository().iterator();
+					it
+					.hasNext();) {
+
 				CRResolvableBean chBean = (CRResolvableBean) it.next();
-	
+
 				JSONObject chElement = processElement(chBean);
 				childContainer.add(chElement);
-				//childContainer.element(chBean.getContentid(),chElement);
+				// childContainer.element(chBean.getContentid(),chElement);
 			}
-			objElement.element("children",childContainer);
+			objElement.element("children", childContainer);
 		}
 		return objElement;
 	}
 
+	/**
+	 * 
+	 * @param bValue
+	 * @return
+	 */
 	private String convertToString(Object bValue) {
 		String value;
-		if(bValue instanceof String) {							
-			value=(String)bValue;
+		if (bValue instanceof String) {
+			value = (String) bValue;
 		} else if (bValue instanceof Number) {
 			value = bValue.toString();
 		} else if (bValue instanceof Date) {
 			value = Long.toString(((Date) bValue).getTime());
 		} else {
-			byte[] bs = (byte[])bValue;
-			value=new String(bs);								
+			byte[] bs = (byte[]) bValue;
+			value = new String(bs);
+
 		}
 		return value;
 	}
-	
-	
+
 }
