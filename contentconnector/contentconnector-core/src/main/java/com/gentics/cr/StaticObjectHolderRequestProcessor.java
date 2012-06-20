cb8c158e7317704d4e54741138e9a514711cdf76
bigbear.ap@gmail.com <bigbear.ap@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2012-06-20 12:16:51 +0000

    implement simple filtering for the StaticObjectHolderRequestProcessor

--- contentconnector/contentconnector-core/src/main/java/com/gentics/cr/StaticObjectHolderRequestProcessor.java
+++ contentconnector/contentconnector-core/src/main/java/com/gentics/cr/StaticObjectHolderRequestProcessor.java
@@ -1,9 +1,15 @@
 package com.gentics.cr;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Vector;
 
 import com.gentics.cr.exceptions.CRException;
+import com.gentics.cr.portalnode.expressions.ExpressionParserHelper;
+import com.gentics.api.lib.exception.ParserException;
+import com.gentics.api.lib.expressionparser.Expression;
+import com.gentics.api.lib.expressionparser.ExpressionParserException;
 
 /**
  * This is a simple RequestProcessor that just holds a static array of CRResolvables in the JVM. 
@@ -32,7 +38,23 @@ public class StaticObjectHolderRequestProcessor extends RequestProcessor {
 
 	@Override
 	public Collection<CRResolvableBean> getObjects(final CRRequest request, final boolean doNavigation) throws CRException {
-		//TODO add the possibility to filter the objects with the request
+		String filter = request.getRequestFilter();
+		if (filter != null && !filter.equals("")) {
+			try {
+				Expression expression = ExpressionParserHelper.parse(filter);
+				ArrayList<CRResolvableBean> result = new ArrayList<CRResolvableBean>();
+				for (CRResolvableBean object : objects) {
+					if (ExpressionParserHelper.match(expression, object)) {
+						result.add(object);
+					}
+				}
+				return result;
+			} catch (ParserException e) {
+				throw new CRException(e);
+			} catch (ExpressionParserException e) {
+				throw new CRException(e);
+			}
+		}
 		return objects;
 	}
 
@@ -45,7 +67,7 @@ public class StaticObjectHolderRequestProcessor extends RequestProcessor {
 	 * all threads it should be thread safe if you plan to manipulate in any way.
 	 */
 	public static void setObjects(final Collection<CRResolvableBean> newObjects) {
-		objects = newObjects;
+		objects = Collections.synchronizedCollection(newObjects);
 	}
 
 }
