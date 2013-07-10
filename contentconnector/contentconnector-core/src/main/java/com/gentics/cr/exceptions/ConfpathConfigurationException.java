7875286e0ae6b01ce0ff0fcfac6a7e6f8aca3995
Sebastian Vogel <s.vogel@gentics.com>
2013-07-10 15:48:19 +0200

    added javadoc

--- contentconnector/contentconnector-core/src/main/java/com/gentics/cr/exceptions/ConfpathConfigurationException.java
+++ contentconnector/contentconnector-core/src/main/java/com/gentics/cr/exceptions/ConfpathConfigurationException.java
@@ -1,20 +1,25 @@
 package com.gentics.cr.exceptions;
-
+/**
+ * This exception is thrown when the system property
+ * com.gentics.portalnode.confpath is not configured correctly
+ * 
+ * @author Sebastian Vogel <s.vogel@gentics.com>
+ */
 public class ConfpathConfigurationException extends RuntimeException {
-
+	/**
+	 * auto generated serialVersionUID
+	 */
+	private static final long serialVersionUID = 1944153187045237753L;
+	
 	public ConfpathConfigurationException(String message) {
 		super(message);	
+	}
+	
+	public ConfpathConfigurationException(Throwable cause) {
+		super(cause);
 	}
 	
 	public ConfpathConfigurationException(String message, Throwable cause) {
 		super(message, cause);
 	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1944153187045237753L;
-	
-	
-
 }
