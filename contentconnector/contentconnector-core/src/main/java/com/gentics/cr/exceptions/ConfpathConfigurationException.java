e77e376bfabe06fe76293023378bda31793037f5
Sebastian Vogel <s.vogel@gentics.com>
2013-07-10 15:20:20 +0200

    added classpath resolving for portalnode confpath

---
+++ contentconnector/contentconnector-core/src/main/java/com/gentics/cr/exceptions/ConfpathConfigurationException.java
@@ -0,0 +1,20 @@
+package com.gentics.cr.exceptions;
+
+public class ConfpathConfigurationException extends RuntimeException {
+
+	public ConfpathConfigurationException(String message) {
+		super(message);	
+	}
+	
+	public ConfpathConfigurationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1944153187045237753L;
+	
+	
+
+}
