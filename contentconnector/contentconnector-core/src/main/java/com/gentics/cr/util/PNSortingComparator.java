2a041b5a482ef79c7f66af17407ce53313458b07
Christopher Supnig <c.supnig@gentics.com>
2013-07-17 16:07:33 +0200

    added new constructor to pnsortingcomparator and added tests

--- contentconnector/contentconnector-core/src/main/java/com/gentics/cr/util/PNSortingComparator.java
+++ contentconnector/contentconnector-core/src/main/java/com/gentics/cr/util/PNSortingComparator.java
@@ -3,16 +3,37 @@ package com.gentics.cr.util;
 import java.util.Comparator;
 
 import com.gentics.api.lib.datasource.Datasource;
+import com.gentics.api.lib.datasource.Datasource.Sorting;
 import com.gentics.api.lib.resolving.Resolvable;
 
+/**
+ * A Comparator for sorting collections of Resolvables by a given Sorting.
+ * @author Christopher
+ *
+ * @param <T>
+ */
 public class PNSortingComparator<T extends Resolvable> implements Comparator<Resolvable> {
 
 	private String columnName;
 	private int sortOrder;
 	
+	/**
+	 * Constructor with column name and sort order (Datasource.SORTORDER_DESC/Datasource.SORTORDER_ASC).
+	 * @param columnName
+	 * @param sortOrder
+	 */
 	public PNSortingComparator(String columnName, int sortOrder) {
 		this.columnName = columnName;
 		this.sortOrder = sortOrder;
+	}
+	
+	/**
+	 * Constructor with Sorting.
+	 * @param sorting
+	 */
+	public PNSortingComparator(Sorting sorting) {
+		this.columnName = sorting.getColumnName();
+		this.sortOrder = sorting.getSortOrder();
 	}
 	
 	
