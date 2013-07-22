2fe322f90b9d8eef8a90c8e7b6720401dc32c2b0
Christopher Supnig <c.supnig@gentics.com>
2013-07-22 08:56:35 +0200

    added quick test to the abstract transformer test to see if the confpath is set

--- contentconnector/contentconnector-poi-transformer/src/test/java/com/gentics/cr/lucene/indexer/transformer/AbstractTransformerTest.java
+++ contentconnector/contentconnector-poi-transformer/src/test/java/com/gentics/cr/lucene/indexer/transformer/AbstractTransformerTest.java
@@ -3,11 +3,15 @@ package com.gentics.cr.lucene.indexer.transformer;
 import java.io.File;
 import java.net.URISyntaxException;
 
+import junit.framework.Assert;
+
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.gentics.DefaultTestConfiguration;
 import com.gentics.cr.configuration.EnvironmentConfiguration;
 import com.gentics.cr.exceptions.CRException;
+import com.gentics.cr.util.CRUtil;
 
 public class AbstractTransformerTest {
 
@@ -15,5 +19,11 @@ public class AbstractTransformerTest {
 	public static void setUp() throws CRException, URISyntaxException {
 		EnvironmentConfiguration.setConfigPath(new File(DefaultTestConfiguration.class.getResource("conf/gentics").getFile()).getAbsolutePath());
 		EnvironmentConfiguration.loadEnvironmentProperties();
+	}
+	
+	@Test
+	public void quickTest() {
+		String prop = System.getProperty(CRUtil.PORTALNODE_CONFPATH);
+		Assert.assertNotNull(prop);
 	}
 }
