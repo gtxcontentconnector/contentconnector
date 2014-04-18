71288a70aa2ba3b2f8154b01818ac99c0e46fd01
Johannes Schüth <j.schueth@gentics.com>
2014-04-18 16:09:07 +0200

    Add changelog module for gentics content connector.

---
+++ contentconnector-changelog/upload-changelog.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+REMOTE_HOST=gentics@www.gentics.com
+REMOTE_DEST=/var/www/vhosts/www.gentics.com/gcn
+TMPDIR=target
+
+SCRIPT="`readlink -f $0`" 
+BASEDIR="`dirname "$SCRIPT"`"
+
+cd $BASEDIR
+
+# Generic error check and abort method
+function handleError() {
+  if [ "$1" != "0" ]; then
+     echo -e "\n\nERROR: $2"
+     echo -e "Aborting with errorcode $1 \n\n"
+     exit 10
+  fi
+}
+
+
+
+echo -e "\n * Uploading Changelog. Press enter to continue"
+read
+  
+  echo -e "\n * Cleanup"
+    rm $TMPDIR/*single*.zip
+    ssh $REMOTE_HOST "cd $REMOTE_DEST/changelog ; rm *.zip"
+    handleError $? "Could not cleanup remote changelog directory"
+  echo "Done."
+  
+  echo -e "\n * Transfering and extracting changelog"
+    scp $TMPDIR/*.zip  $REMOTE_HOST:$REMOTE_DEST/changelog
+    handleError $? "Could not transfer changelog"
+    ssh $REMOTE_HOST "cd $REMOTE_DEST/changelog ; unzip -o *.zip" 
+    handleError $? "Could not extract changelog"
+  echo "Done."
+
+echo "Done."
